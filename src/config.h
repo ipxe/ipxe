8e4c991c24104361c71e8d42ed455083d9d99cec
Michael Brown <mcb30@etherboot.org>
2005-04-17 14:08:40 +0000

    Experiment; let's see if we can get individual config header files working.

---
+++ src/config.h
@@ -0,0 +1,36 @@
+/*
+ * This file defines the configuration for Etherboot.
+ *
+ * The build system splits this file into several individual header
+ * files, so that changing one option doesn't necessitate a rebuild of
+ * every single object.  For this reason, it is important to maintain
+ * the strict formatting in this file.
+ *
+ */
+
+/* BEGIN config_serial.h
+ *
+ * Serial port configuration
+ *
+ * These options affect the operation of the serial console.  They
+ * take effect only if the serial console is included using the
+ * CONSOLE_SERIAL option.
+ *
+ */
+
+#define COMCONSOLE	0x3f8		/* I/O port address */
+
+/* Keep settings from a previous user of the serial port (e.g. lilo or
+ * LinuxBIOS), ignoring COMSPEED, COMDATA, COMPARITY and COMSTOP.
+ */
+#define COMPRESERVE	0
+
+#if ! COMPRESERVE
+#define COMSPEED	9600		/* Baud rate */
+#define COMDATA		8		/* Data bits */ 
+#define COMPARITY	N		/* Parity */
+#define COMSTOP		1		/* Stop bits */
+#endif
+
+/* END config_serial.h */
+
