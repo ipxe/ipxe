c245ab75ad4fadd577fb5d7a5ac326ea761d121e
Michael Brown <mcb30@etherboot.org>
2005-04-18 15:35:39 +0000

    Moved disk functions to drivers/bus/bios_disks.c

--- src/arch/i386/include/bios.h
+++ src/arch/i386/include/bios.h
@@ -3,8 +3,5 @@
 
 extern unsigned long currticks ( void );
 extern void cpu_nap ( void );
-extern void disk_init ( void );
-extern unsigned int pcbios_disk_read ( int drive, int cylinder, int head,
-				       int sector, char *fixme_buf );
 
 #endif /* BIOS_H */
