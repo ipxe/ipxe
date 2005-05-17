9c832a6a69aa4cc15bbfe3bfe4677dd66b79fe42
Michael Brown <mcb30@etherboot.org>
2005-05-17 13:41:00 +0000

    First version

---
+++ src/arch/i386/include/gateA20.h
@@ -0,0 +1,7 @@
+#ifndef GATEA20_H
+#define GATEA20_H
+
+extern void gateA20_set ( void );
+extern void gateA20_unset ( void );
+
+#endif /* GATEA20_H */
---
+++ src/include/osloader.h
@@ -0,0 +1,6 @@
+#ifndef OSLOADER_H
+#define OSLOADER_H
+
+extern int prep_segment ( physaddr_t start, physaddr_t mid, physaddr_t end );
+
+#endif /* OSLOADER_H */
