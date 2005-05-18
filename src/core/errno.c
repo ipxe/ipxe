d04b607eb9b57a51359720d8f1d5e8bad949e61b
Michael Brown <mcb30@etherboot.org>
2005-05-18 14:44:38 +0000

    Add doxygen documentation

--- src/core/errno.c
+++ src/core/errno.c
@@ -1,17 +1,38 @@
+#include "etherboot.h"
 #include "errno.h"
 #include "vsprintf.h"
 
-/* Global "last error" number */
+/** @file
+ *
+ * Error codes and descriptions.
+ *
+ * This file provides the global variable errno
+ *
+ */
+
+/**
+ * Global "last error" number.
+ *
+ * This is valid only when a function has just returned indicating a
+ * failure.
+ *
+ */
 int errno;
 
 static struct errortab errortab_start[0] __table_start(errortab);
 static struct errortab errortab_end[0] __table_end(errortab);
 
-/*
+/**
  * Retrieve string representation of error number.
  *
- * If error not found in the error table, generate a generic "Error
- * 0x0000" message.
+ * @v errno		Error number
+ * @ret strerror	Pointer to error text
+ *
+ * If the error is not found in the linked-in error tables, generates
+ * a generic "Error 0x0000" message.
+ *
+ * The pointer returned by strerror() is valid only until the next
+ * call to strerror().
  *
  */
 const char * strerror ( int errno ) {
