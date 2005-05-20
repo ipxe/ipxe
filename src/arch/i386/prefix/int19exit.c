d5789ff4a2a2c8c9aa4fa7efbab4600afd092f89
Michael Brown <mcb30@etherboot.org>
2005-05-20 11:28:27 +0000

    Doxygenation

--- src/arch/i386/prefix/int19exit.c
+++ src/arch/i386/prefix/int19exit.c
@@ -1,12 +1,26 @@
 #include "bochs.h"
 #include "realmode.h"
 
-/**
+/** @file
+ *
  * The "exit via INT 19" exit path.
  *
  * INT 19 is the old (pre-BBS) "boot system" interrupt.  It is
  * conventionally used now to return from a failed boot from floppy
  * disk.
+ *
+ */
+
+/**
+ * Exit via INT19
+ *
+ * @v ix86		i386 register values to be loaded on exit
+ * @ret Never
+ * @err None
+ *
+ * Exit back to the BIOS by switching to real mode, reloading the
+ * registers as they were before Etherboot started, and executing INT
+ * 19.
  *
  * @bug Not yet implemented
  *
