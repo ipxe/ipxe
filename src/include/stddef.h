508dcdb3035a2ef826e0feda39c7648df25c88ad
Michael Brown <mcb30@etherboot.org>
2006-03-23 22:07:21 +0000

    Added container_of().  This seems about the best place to put it, since the definition of it uses offsetof(), and the two concepts are related.

--- src/include/stddef.h
+++ src/include/stddef.h
@@ -2,12 +2,17 @@
 #define STDDEF_H
 
 /* for size_t */
-#include "stdint.h"
+#include <stdint.h>
 
 #undef NULL
-#define NULL	((void *)0)
+#define NULL ((void *)0)
 
 #undef offsetof
 #define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+
+#undef container_of
+#define container_of(ptr, type, member) ({                      \
+	const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+	(type *)( (char *)__mptr - offsetof(type,member) );})
 
 #endif /* STDDEF_H */
