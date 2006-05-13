8638a5e4719cbea018ebe24340f2cdcb3bc70582
Michael Brown <mcb30@etherboot.org>
2006-05-13 11:36:30 +0000

    Defined a block device interface.

---
+++ src/include/gpxe/blockdev.h
@@ -0,0 +1,39 @@
+#ifndef _GPXE_BLOCKDEV_H
+#define _GPXE_BLOCKDEV_H
+
+/**
+ * @file
+ *
+ * Block devices
+ *
+ */
+
+/** A block device */
+struct block_device {
+	/** Block size */
+	size_t blksize;
+	/** Total number of blocks */
+	uint64_t blocks;
+	/**
+	 * Read block
+	 *
+	 * @v blockdev	Block device
+	 * @v block	Block number
+	 * @v buffer	Data buffer
+	 * @ret rc	Return status code
+	 */
+	int ( * read ) ( struct block_device *blockdev, uint64_t block,
+			 void *buffer );
+	/**
+	 * Write block
+	 *
+	 * @v blockdev	Block device
+	 * @v block	Block number
+	 * @v buffer	Data buffer
+	 * @ret rc	Return status code
+	 */
+	int ( * write ) ( struct block_device *blockdev, uint64_t block,
+			  const void *buffer );
+};
+
+#endif /* _GPXE_BLOCKDEV_H */
