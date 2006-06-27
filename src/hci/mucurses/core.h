9ca2f64ee1ec4606f57e4437b43fd1459e0679cc
Dan Lynch <danjlynch@users.sourceforge.net>
2006-06-27 13:31:30 +0000

    moved console specific items out of mucurses (should be handled by external screen object)

--- src/hci/mucurses/core.h
+++ src/hci/mucurses/core.h
@@ -10,15 +10,6 @@
 #define WRAP 0
 #define NOWRAP 1
 
-unsigned short _COLS;
-unsigned short _LINES;
-unsigned short _COLOURS;
-unsigned int *_COLOUR_PAIRS; /* basically this is an array, but as its
-			       length is determined only when initscr
-			       is run, I can only think to make it a
-			       pointer and malloc the array into being
-			       ... */
-
 void _wputch ( WINDOW *win, chtype ch, int wrap );
 void _wputchstr ( WINDOW *win, const chtype *chstr, int wrap, int n );
 void _wputstr ( WINDOW *win, const char *str, int wrap, int n );
