b8d619e82295d9934d6223e58b75ba862163ea0b
Michael Brown <mcb30@etherboot.org>
2006-07-19 16:16:51 +0000

    Added macros for generating static DHCP options

--- src/include/gpxe/dhcp.h
+++ src/include/gpxe/dhcp.h
@@ -10,6 +10,7 @@
 #include <stdint.h>
 #include <gpxe/list.h>
 #include <gpxe/in.h>
+#include <gpxe/udp.h>
 
 /** Construct a tag value for an encapsulated option
  *
@@ -126,6 +127,54 @@
 #define DHCP_END 255
 
 /** @} */
+
+/**
+ * Count number of arguments to a variadic macro
+ *
+ * This rather neat, non-iterative solution is courtesy of Laurent
+ * Deniau.
+ *
+ */
+#define _VA_ARG_COUNT(  _1,  _2,  _3,  _4,  _5,  _6,  _7,  _8,		\
+		        _9, _10, _11, _12, _13, _14, _15, _16,		\
+		       _17, _18, _19, _20, _21, _22, _23, _24,		\
+		       _25, _26, _27, _28, _29, _30, _31, _32,		\
+		       _33, _34, _35, _36, _37, _38, _39, _40,		\
+		       _41, _42, _43, _44, _45, _46, _47, _48,		\
+		       _49, _50, _51, _52, _53, _54, _55, _56,		\
+		       _57, _58, _59, _60, _61, _62, _63,   N, ... ) N
+#define VA_ARG_COUNT( ... )						\
+	_VA_ARG_COUNT ( __VA_ARGS__, 					\
+			63, 62, 61, 60, 59, 58, 57, 56,			\
+			55, 54, 53, 52, 51, 50, 49, 48,			\
+			47, 46, 45, 44, 43, 42, 41, 40,			\
+			39, 38, 37, 36, 35, 34, 33, 32,			\
+			31, 30, 29, 28, 27, 26, 25, 24,			\
+			23, 22, 21, 20, 19, 18, 17, 16,			\
+			15, 14, 13, 12, 11, 10,  9,  8,			\
+			 7,  6,  5,  4,  3,  2,  1,  0 )
+
+/** Construct a DHCP option from a list of bytes */
+#define DHCP_OPTION( ... ) VA_ARG_COUNT ( __VA_ARGS__ ), __VA_ARGS__
+
+/** Construct a DHCP option from a list of characters */
+#define DHCP_STRING( ... ) DHCP_OPTION ( __VA_ARGS__ )
+
+/** Construct a byte-valued DHCP option */
+#define DHCP_BYTE( value ) DHCP_OPTION ( value )
+
+/** Construct a word-valued DHCP option */
+#define DHCP_WORD( value ) DHCP_OPTION ( ( ( (value) >> 8 ) & 0xff ),   \
+					 ( ( (value) >> 0  ) & 0xff ) )
+
+/** Construct a dword-valued DHCP option */
+#define DHCP_DWORD( value ) DHCP_OPTION ( ( ( (value) >> 24 ) & 0xff ), \
+					  ( ( (value) >> 16 ) & 0xff ), \
+					  ( ( (value) >> 8  ) & 0xff ), \
+					  ( ( (value) >> 0  ) & 0xff ) )
+
+/** Construct a DHCP encapsulated options field */
+#define DHCP_ENCAP( ... ) DHCP_OPTION ( __VA_ARGS__, DHCP_END )
 
 /**
  * A DHCP option
@@ -313,12 +362,23 @@ struct dhcp_packet {
 
 /** A DHCP session */
 struct dhcp_session {
+	/** UDP connection for this session */
+	struct udp_connection udp;
+
+	/** State of the session
+	 *
+	 * This is a value for the @c DHCP_MESSAGE_TYPE option
+	 * (e.g. @c DHCPDISCOVER).
+	 */
+	int state;
+	
 	/** Network device being configured */
 	struct net_device *netdev;
 	/** Transaction ID, in network-endian order */
 	uint32_t xid;
 };
 
+extern const struct dhcp_option_block dhcp_request_options;
 extern unsigned long dhcp_num_option ( struct dhcp_option *option );
 extern struct dhcp_option *
 find_dhcp_option ( struct dhcp_option_block *options, unsigned int tag );
