c46126bfa4c9e13e88df14c0a9a2055b9e581e47
Michael Brown <mcb30@etherboot.org>
2006-08-27 01:39:11 +0000

    Does anyone still use 9600 baud?

--- src/config.h
+++ src/config.h
@@ -43,7 +43,7 @@
 #undef	COMPRESERVE
 
 #ifndef COMPRESERVE
-#define	COMSPEED	9600		/* Baud rate */
+#define	COMSPEED	115200		/* Baud rate */
 #define	COMDATA		8		/* Data bits */ 
 #define	COMPARITY	0		/* Parity: 0=None, 1=Odd, 2=Even */
 #define	COMSTOP		1		/* Stop bits */
