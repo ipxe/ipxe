bb8725bffa6cc15d0f37f7b63e229692d6ec2413
Michael Brown <mcb30@etherboot.org>
2006-09-11 17:54:19 +0000

    Added the "swap in place" macros

--- src/include/little_bswap.h
+++ src/include/little_bswap.h
@@ -17,5 +17,17 @@
 #define be64_to_cpu(x)	__bswap_64(x)
 #define be32_to_cpu(x)	__bswap_32(x)
 #define be16_to_cpu(x)	__bswap_16(x)
+#define cpu_to_le64s(x) do {} while (0)
+#define cpu_to_le32s(x) do {} while (0)
+#define cpu_to_le16s(x) do {} while (0)
+#define cpu_to_be64s(x) __bswap_64s(x)
+#define cpu_to_be32s(x) __bswap_32s(x)
+#define cpu_to_be16s(x) __bswap_16s(x)
+#define le64_to_cpus(x) do {} while (0)
+#define le32_to_cpus(x) do {} while (0)
+#define le16_to_cpus(x) do {} while (0)
+#define be64_to_cpus(x) __bswap_64s(x)
+#define be32_to_cpus(x) __bswap_32s(x)
+#define be16_to_cpus(x) __bswap_16s(x)
 
 #endif /* ETHERBOOT_LITTLE_BSWAP_H */
