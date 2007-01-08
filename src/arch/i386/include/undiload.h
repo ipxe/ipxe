61153568590011d3a80f0eb3ad9becf7e500db72
Michael Brown <mcb30@etherboot.org>
2007-01-08 02:51:27 +0000

    Added missing declarations

--- src/arch/i386/include/undiload.h
+++ src/arch/i386/include/undiload.h
@@ -7,6 +7,9 @@
  *
  */
 
+struct undi_device;
+struct undi_rom;
+
 extern int undi_load_pci ( struct undi_device *undi, struct undi_rom *undirom,
 			   unsigned int bus, unsigned int devfn );
 extern int undi_unload ( struct undi_device *undi );
