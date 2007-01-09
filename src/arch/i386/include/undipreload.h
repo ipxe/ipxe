937289cdc8606003b575194f2af2a50923e90e31
Michael Brown <mcb30@etherboot.org>
2007-01-09 02:29:18 +0000

    Provide storage for an undi_device structure representing the preloaded UNDI stack.

---
+++ src/arch/i386/include/undipreload.h
@@ -0,0 +1,16 @@
+#ifndef _UNDIPRELOAD_H
+#define _UNDIPRELOAD_H
+
+/** @file
+ *
+ * Preloaded UNDI stack
+ *
+ */
+
+#include <realmode.h>
+#include <undi.h>
+
+extern struct undi_device __data16 ( preloaded_undi );
+#define preloaded_undi __use_data16 ( preloaded_undi )
+
+#endif /* _UNDIPRELOAD_H */
