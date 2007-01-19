a213876382091d38ac8c0bb10d0357a12fa7d7b4
Michael Brown <mcb30@etherboot.org>
2007-01-19 00:48:13 +0000

    Typo

--- src/tests/umalloc_test.c
+++ src/tests/umalloc_test.c
@@ -11,9 +11,9 @@ void umalloc_test ( void ) {
 	printf ( "Before allocation:\n" );
 	get_memmap ( &memmap );
 
-	bob = ymalloc ( 1234 );
-	bob = yrealloc ( bob, 12345 );
-	fred = ymalloc ( 999 );
+	bob = umalloc ( 1234 );
+	bob = urealloc ( bob, 12345 );
+	fred = umalloc ( 999 );
 
 	printf ( "After allocation:\n" );
 	get_memmap ( &memmap );
