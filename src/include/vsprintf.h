06475f7b69dc15551d41f6eb3b20409a8a1f34c6
Michael Brown <mcb30@etherboot.org>
2007-01-19 00:58:49 +0000

    Finally move the prototypes for printf() and friends to stdio.h

--- src/include/vsprintf.h
+++ src/include/vsprintf.h
@@ -33,8 +33,7 @@
 
 #include <stdint.h>
 #include <stdarg.h>
-
-#define PRINTF_NO_LENGTH ( ( size_t ) -1 )
+#include <stdio.h>
 
 /**
  * A printf context
@@ -65,36 +64,4 @@ struct printf_context {
 
 extern size_t vcprintf ( struct printf_context *ctx, const char *fmt,
 			 va_list args );
-extern int vsnprintf ( char *buf, size_t size, const char *fmt, va_list args );
-extern int vprintf ( const char *fmt, va_list args );
-
-extern int __attribute__ (( format ( printf, 3, 4 ) ))
-snprintf ( char *buf, size_t size, const char *fmt, ... );
-
-extern int __attribute__ (( format ( printf, 1, 2 ) ))
-printf ( const char *fmt, ... );
-
-/**
- * Write a formatted string to a buffer
- *
- * @v buf		Buffer into which to write the string
- * @v fmt		Format string
- * @v ...		Arguments corresponding to the format string
- * @ret len		Length of formatted string
- */
-#define sprintf( buf, fmt, ... ) \
-	snprintf ( (buf), PRINTF_NO_LENGTH, (fmt), ## __VA_ARGS__ )
-
-/**
- * Write a formatted string to a buffer
- *
- * @v buf		Buffer into which to write the string
- * @v fmt		Format string
- * @v args		Arguments corresponding to the format string
- * @ret len		Length of formatted string
- */
-static inline int vsprintf ( char *buf, const char *fmt, va_list args ) {
-	return vsnprintf ( buf, PRINTF_NO_LENGTH, fmt, args );
-}
-
 #endif /* VSPRINTF_H */
