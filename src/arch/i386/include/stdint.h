f59281d8f645d2fb6225ca27a3a8e7db5ceb81e5
Michael Brown <mcb30@etherboot.org>
2007-01-26 01:30:16 +0000

    Yet more synonyms that people tend to use.

--- src/arch/i386/include/stdint.h
+++ src/arch/i386/include/stdint.h
@@ -18,16 +18,22 @@ typedef signed long long	int64_t;
 typedef unsigned long		physaddr_t;
 typedef unsigned long		intptr_t;
 
-typedef signed char	   s8;
-typedef unsigned char      u8;
+typedef int8_t s8;
+typedef uint8_t u8;
+typedef int16_t s16;
+typedef uint16_t u16;
+typedef int32_t s32;
+typedef uint32_t u32;
+typedef int64_t s64;
+typedef uint64_t u64;
 
-typedef signed short       s16;
-typedef unsigned short     u16;
-
-typedef signed int         s32;
-typedef unsigned int       u32;
-
-typedef signed long long   s64;
-typedef unsigned long long u64;
+typedef int8_t int8;
+typedef uint8_t uint8;
+typedef int16_t int16;
+typedef uint16_t uint16;
+typedef int32_t int32;
+typedef uint32_t uint32;
+typedef int64_t int64;
+typedef uint64_t uint64;
 
 #endif /* STDINT_H */
