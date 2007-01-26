bf9ec8102f78562ebf47400522c375b0c9235206
Michael Brown <mcb30@etherboot.org>
2007-01-26 02:53:30 +0000

    Add 64-bit rotates

--- src/include/gpxe/bitops.h
+++ src/include/gpxe/bitops.h
@@ -16,4 +16,12 @@ static inline uint32_t ror32 ( uint32_t data, unsigned int rotation ) {
         return ( ( data >> rotation ) | ( data << ( 32 - rotation ) ) );
 }
 
+static inline uint64_t rol64 ( uint64_t data, unsigned int rotation ) {
+        return ( ( data << rotation ) | ( data >> ( 64 - rotation ) ) );
+}
+
+static inline uint64_t ror64 ( uint64_t data, unsigned int rotation ) {
+        return ( ( data >> rotation ) | ( data << ( 64 - rotation ) ) );
+}
+
 #endif /* _GPXE_BITOPS_H */
