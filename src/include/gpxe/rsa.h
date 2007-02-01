a1f021db96662b58c4f9dcd2a34578b983489caa
Michael Brown <mcb30@etherboot.org>
2007-02-01 02:17:33 +0000

    Placeholder

---
+++ src/include/gpxe/rsa.h
@@ -0,0 +1,10 @@
+#ifndef _GPXE_RSA_H
+#define _GPXE_RSA_H
+
+struct crypto_algorithm;
+
+extern struct crypto_algorithm rsa_algorithm;
+
+#include "crypto/axtls/crypto.h"
+
+#endif /* _GPXE_RSA_H */
