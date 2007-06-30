d5451d210cc23cc0788911d62c9cee9a40ecae79
Michael Brown <mcb30@etherboot.org>
2007-06-30 22:59:15 +0100

    Add ability to resolve relative symbols as well as locate absolute ones.

--- src/arch/i386/core/gdbsym.c
+++ src/arch/i386/core/gdbsym.c
@@ -15,6 +15,7 @@ static void gdb_symbol_line ( void ) {
 	printf ( "Commands to start up gdb:\n\n" );
 	printf ( "gdb\n" );
 	printf ( "target remote localhost:1234\n" );
+	printf ( "set confirm off\n" );
 	printf ( "add-symbol-file symbols %#lx", virt_to_phys ( __text ) );
 	printf ( " -s .rodata %#lx", virt_to_phys ( __rodata ) );
 	printf ( " -s .data %#lx", virt_to_phys ( __data ) );
@@ -22,6 +23,8 @@ static void gdb_symbol_line ( void ) {
 	printf ( " -s .text16 %#x", ( ( rm_cs << 4 ) + (int)__text16 ) );
 	printf ( " -s .data16 %#x", ( ( rm_ds << 4 ) + (int)__data16 ) );
 	printf ( "\n" );
+	printf ( "add-symbol-file symbols 0\n" );
+	printf ( "set confirm on\n" );
 	getkey();
 }
 
