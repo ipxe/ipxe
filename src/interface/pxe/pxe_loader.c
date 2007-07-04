0324d5ff709b0669396c872814004e8f2d86e86c
Michael Brown <mcb30@etherboot.org>
2007-07-04 03:20:18 +0100

    !PXE and PXENV+ structures are in code segment, not data segment.

--- src/interface/pxe/pxe_loader.c
+++ src/interface/pxe/pxe_loader.c
@@ -31,7 +31,8 @@
  */
 PXENV_EXIT_t undi_loader ( struct s_UNDI_LOADER *undi_loader ) {
 
-	DBG ( "[PXENV_UNDI_LOADER]" );
+	DBG ( "[PXENV_UNDI_LOADER to CS %04x DS %04x]",
+	      undi_loader->UNDI_CS, undi_loader->UNDI_DS );
 
 	/* Perform one-time initialisation (e.g. heap) */
 	initialise();
@@ -40,10 +41,10 @@ PXENV_EXIT_t undi_loader ( struct s_UNDI_LOADER *undi_loader ) {
 	pxe_init_structures();
 
 	/* Fill in UNDI loader structure */
-	undi_loader->PXEptr.segment = rm_ds;
+	undi_loader->PXEptr.segment = rm_cs;
 	undi_loader->PXEptr.offset =
 		( ( unsigned ) & __from_text16 ( ppxe ) );
-	undi_loader->PXENVptr.segment = rm_ds;
+	undi_loader->PXENVptr.segment = rm_cs;
 	undi_loader->PXENVptr.offset =
 		( ( unsigned ) & __from_text16 ( pxenv ) );
 
