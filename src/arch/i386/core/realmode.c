dc52db49202e333a7b1aafcf73df3c612d0638ae
Michael Brown <mcb30@etherboot.org>
2007-07-17 01:43:19 +0100

    Removed dead code identified by symcheck

--- src/arch/i386/core/realmode.c
+++ src/arch/i386/core/realmode.c
@@ -21,20 +21,3 @@ void memcpy_from_real ( void *dest, segoff_t src, size_t n ) {
 }
 
 #endif /* KEEP_IT_REAL */
-
-
-#define RM_STACK_SIZE ( 0x1000 )
-
-/* gcc won't let us use extended asm outside a function (compiler
- * bug), ao we have to put these asm statements inside a dummy
- * function.
- */
-static void work_around_gcc_bug ( void ) __attribute__ ((used));
-static void work_around_gcc_bug ( void ) {
-	/* Export _real_mode_stack_size as absolute linker symbol */
-	__asm__ ( ".globl real_mode_stack_size" );
-	__asm__ ( ".equ real_mode_stack_size, %c0" : : "i" (RM_STACK_SIZE) );
-}
-
-char *real_mode_stack;
-int lock_real_mode_stack;
