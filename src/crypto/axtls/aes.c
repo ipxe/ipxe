426c2c150d43f1c2661205c9a0956fcbeeaeacc4
Holger Lubitz <hal@duncan.ol.sub.de>
2007-07-27 21:31:11 +0200

    disable AES_convert_key by #if 0'ing it out (suggested by mcb30)

--- src/crypto/axtls/aes.c
+++ src/crypto/axtls/aes.c
@@ -238,6 +238,9 @@ void AES_set_key(AES_CTX *ctx, const uint8_t *key,
     memcpy(ctx->iv, iv, 16);
 }
 
+#if 0
+/** currently unused function **/
+
 /**
  * Change a key for decryption.
  */
@@ -256,6 +259,7 @@ void AES_convert_key(AES_CTX *ctx)
         *k++ =w;
     }
 }
+#endif
 
 /**
  * Encrypt a byte sequence (with a block size 16) using the AES cipher.
