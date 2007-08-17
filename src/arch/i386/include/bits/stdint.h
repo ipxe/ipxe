d0974ec7166c42be774d32df954a0e71283fa245
Michael Brown <mcb30@etherboot.org>
2007-08-17 19:51:08 +0100

    Separate out arch-independent parts of stdint.h

--- src/arch/i386/include/bits/stdint.h
+++ src/arch/i386/include/bits/stdint.h
@@ -1,5 +1,5 @@
-#ifndef STDINT_H
-#define STDINT_H
+#ifndef _BITS_STDINT_H
+#define _BITS_STDINT_H
 
 typedef typeof(sizeof(int))	size_t;
 typedef signed long		ssize_t;
@@ -18,22 +18,4 @@ typedef signed long long	int64_t;
 typedef unsigned long		physaddr_t;
 typedef unsigned long		intptr_t;
 
-typedef int8_t s8;
-typedef uint8_t u8;
-typedef int16_t s16;
-typedef uint16_t u16;
-typedef int32_t s32;
-typedef uint32_t u32;
-typedef int64_t s64;
-typedef uint64_t u64;
-
-typedef int8_t int8;
-typedef uint8_t uint8;
-typedef int16_t int16;
-typedef uint16_t uint16;
-typedef int32_t int32;
-typedef uint32_t uint32;
-typedef int64_t int64;
-typedef uint64_t uint64;
-
-#endif /* STDINT_H */
+#endif /* _BITS_STDINT_H */
