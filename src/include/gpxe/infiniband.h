440e7926fbfc419115a85b61d3c740f640b68756
Michael Brown <mcb30@etherboot.org>
2007-09-17 05:12:47 +0100

    Dead code removal

--- src/include/gpxe/infiniband.h
+++ src/include/gpxe/infiniband.h
@@ -10,42 +10,6 @@
 #include <stdint.h>
 #include <gpxe/device.h>
 
-
-
-#if 0
-/** Infiniband MAC address length */
-#define IB_ALEN 20
-
-/** An Infiniband MAC address */
-struct ib_mac {
-	/** Queue pair number
-	 *
-	 * MSB must be zero; QPNs are only 24-bit.
-	 */
-	uint32_t qpn;
-	/** Port GID */
-	struct ib_gid gid;
-} __attribute__ (( packed ));
-
-/** Infiniband link-layer header length */
-#define IB_HLEN 4
-
-/** An Infiniband link-layer header */
-struct ibhdr {
-	/** Network-layer protocol */
-	uint16_t proto;
-	/** Reserved, must be zero */
-	uint16_t reserved;
-} __attribute__ (( packed ));
-#endif
-
-
-
-
-
-
-
-
 /** An Infiniband Global Identifier */
 struct ib_gid {
 	uint8_t bytes[16];
@@ -510,32 +474,5 @@ union ib_mad {
 	struct ib_mad_guid_info guid_info;
 	struct ib_mad_port_info port_info;
 } __attribute__ (( packed ));
-
-
-
-
-#if 0
-
-extern struct ll_protocol infiniband_protocol;
-
-extern const char * ib_ntoa ( const void *ll_addr );
-
-/**
- * Allocate Infiniband device
- *
- * @v priv_size		Size of driver private data
- * @ret netdev		Network device, or NULL
- */
-static inline struct net_device * alloc_ibdev ( size_t priv_size ) {
-	struct net_device *netdev;
-
-	netdev = alloc_netdev ( priv_size );
-	if ( netdev ) {
-		netdev->ll_protocol = &infiniband_protocol;
-	}
-	return netdev;
-}
-
-#endif
 
 #endif /* _GPXE_INFINIBAND_H */
