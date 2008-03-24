dbf8a02e8f719390cc862866414781801d602ecf
Michael Brown <mcb30@etherboot.org>
2008-03-24 00:23:11 +0000

    [libc] Use __builtin_va_list et al in stdarg.h
    The home-grown versions don't work properly for static variadic
    functions, when gcc can choose to use a non-standard calling convention.

---
+++ src/include/stdarg.h
@@ -0,0 +1,10 @@
+#ifndef _STDARG_H
+#define _STDARG_H
+
+typedef __builtin_va_list va_list;
+#define va_start( ap, last ) __builtin_va_start ( ap, last )
+#define va_arg( ap, type ) __builtin_va_arg ( ap, type )
+#define va_end( ap ) __builtin_va_end ( ap )
+#define va_copy( dest, src ) __builtin_va_copy ( dest, src )
+
+#endif /* _STDARG_H */
