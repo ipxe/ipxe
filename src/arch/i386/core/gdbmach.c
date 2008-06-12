5504fa9047d3b0c3d7a2c18f4100c0ad6be61069
Stefan Hajnoczi <stefanha@gmail.com>
2008-06-12 18:19:55 +0100

    [GDB] Add copyright header for gdbmach.c

--- src/arch/i386/core/gdbmach.c
+++ src/arch/i386/core/gdbmach.c
@@ -1,9 +1,33 @@
+/*
+ * Copyright (C) 2008 Stefan Hajnoczi <stefanha@gmail.com>.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include <stddef.h>
 #include <stdio.h>
 #include <assert.h>
 #include <virtaddr.h>
 #include <gpxe/gdbstub.h>
 #include <gdbmach.h>
+
+/** @file
+ *
+ * GDB architecture-specific bits for i386
+ *
+ */
 
 enum {
 	DR7_CLEAR = 0x00000400,    /* disable hardware breakpoints */
