7eb555a8ae87202909e5e1c759149bc2375bb9f5
Stefan Hajnoczi <stefanha@gmail.com>
2008-06-16 13:18:20 +0100

    [GDB] Obey flow control when GDB connects.

--- src/core/gdbstub.c
+++ src/core/gdbstub.c
@@ -350,6 +350,10 @@ static void gdbstub_state_wait_ack ( struct gdbstub *stub, char ch ) {
 		stub->parse = gdbstub_state_new;
 	} else if ( ch == '-' ) {
 		gdbstub_tx_packet ( stub ); /* retransmit */
+	} else if ( ch == '$' ) {
+		/* GDB is reconnecting, drop our packet and listen to GDB */
+		stub->trans->send ( "-", 1 );
+		stub->parse = gdbstub_state_new;
 	}
 }
 
