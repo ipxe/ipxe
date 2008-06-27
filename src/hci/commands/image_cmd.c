ipxe/ipxe

commit 4c8501796877e66b0487d776431c92ee8141f6b6

[cmdline] Remove arbitrary limit on the length of image command lines

src/hci/commands/image_cmd.c
@@ -49,16 +49,25 @@ enum image_action {
  */
 static int imgfill_cmdline ( struct image *image, unsigned int nargs, 
 			     char **args ) {
-	char buf[256];
-	size_t used = 0;
+	size_t len;
+	unsigned int i;
 
-	memset ( buf, 0, sizeof ( buf ) );
-	while ( ( used < sizeof ( buf ) ) && nargs-- ) {
-		used += snprintf ( &buf[used], ( sizeof ( buf ) - used ),
-				   " %s", *(args++) );
+	/* Determine total length of command line */
+	len = 1; /* NUL */
+	for ( i = 0 ; i < nargs ; i++ )
+		len += ( 1 /* space */ + strlen ( args[i] ) );
+
+	{
+		char buf[len];
+		char *ptr = buf;
+
+		/* Assemble command line */
+		for ( i = 0 ; i < nargs ; i++ )
+			ptr += sprintf ( ptr, " %s", args[i] );
+		assert ( ptr == ( buf + len - 1 ) );
+
+		return image_set_cmdline ( image, &buf[1] );
 	}
-
-	return image_set_cmdline ( image, &buf[1] );
 }
 
 /**

commit ddf5c8d4d3ae32df0b0d699f4fabb06dbfb37475

[cmdline] Fix image command-line construction for zero-length argument lists
This fixes a bug introduced in commit 4c85017.

src/hci/commands/image_cmd.c
@@ -55,18 +55,21 @@ static int imgfill_cmdline ( struct image *image, unsigned int nargs,
 	/* Determine total length of command line */
 	len = 1; /* NUL */
 	for ( i = 0 ; i < nargs ; i++ )
-		len += ( 1 /* space */ + strlen ( args[i] ) );
+		len += ( 1 /* possible space */ + strlen ( args[i] ) );
 
 	{
 		char buf[len];
 		char *ptr = buf;
 
 		/* Assemble command line */
-		for ( i = 0 ; i < nargs ; i++ )
-			ptr += sprintf ( ptr, " %s", args[i] );
-		assert ( ptr == ( buf + len - 1 ) );
+		buf[0] = '\0';
+		for ( i = 0 ; i < nargs ; i++ ) {
+			ptr += sprintf ( ptr, "%s%s", ( i ? " " : "" ),
+					 args[i] );
+		}
+		assert ( ptr < ( buf + len ) );
 
-		return image_set_cmdline ( image, &buf[1] );
+		return image_set_cmdline ( image, buf );
 	}
 }
 