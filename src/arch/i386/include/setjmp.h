179fa70d34adb90b26ff46abfad5e21a14cf4f32
Daniel Verkamp <daniel@drv.nu>
2008-07-15 12:15:51 -0400

    [libc] Mark setjmp and longjmp as __cdecl

--- src/arch/i386/include/setjmp.h
+++ src/arch/i386/include/setjmp.h
@@ -6,7 +6,7 @@
 #define JBLEN 6
 typedef unsigned long jmp_buf[JBLEN];
 
-extern int setjmp (jmp_buf env);
-extern void longjmp (jmp_buf env, int val);
+extern int __cdecl setjmp (jmp_buf env);
+extern void __cdecl longjmp (jmp_buf env, int val);
 
 #endif /* ETHERBOOT_SETJMP_H */
