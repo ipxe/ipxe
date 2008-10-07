160b950af816d193509d5ce704a8d9e709aab912
Michael Brown <mcb30@etherboot.org>
2008-10-07 22:27:55 +0100

    [libc] Define wchar_t in a gcc-compatible way
    gcc defines the magic __WCHAR_TYPE__ macro, in order to convey
    information about whether or not the user selected -fshort-wchar.

--- src/include/stddef.h
+++ src/include/stddef.h
@@ -15,4 +15,10 @@
 	const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
 	(type *)( (char *)__mptr - offsetof(type,member) );})
 
+/* __WCHAR_TYPE__ is defined by gcc and will change if -fshort-wchar is used */
+#ifndef __WCHAR_TYPE__
+#define __WCHAR_TYPE__ long int
+#endif
+typedef __WCHAR_TYPE__ wchar_t;
+
 #endif /* STDDEF_H */
