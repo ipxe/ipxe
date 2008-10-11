b40b4f2dbf78e188f03845fab84e08c86fd8db94
Michael Brown <mcb30@etherboot.org>
2008-10-12 00:39:57 +0100

    [cs89x0] Simplify obscure loop syntax

--- src/drivers/net/cs89x0.c
+++ src/drivers/net/cs89x0.c
@@ -419,8 +419,8 @@ retry:
 	      ETH_ALEN/2);
 	outw(((t >> 8)&0xFF)|(t << 8), eth_nic_base + TX_FRAME_PORT);
 	outsw(eth_nic_base + TX_FRAME_PORT, p, (s+1)/2);
-	for (sr = sr/2 - (s+1)/2 - ETH_ALEN - 1; sr-- > 0;
-	     outw(0, eth_nic_base + TX_FRAME_PORT));
+	for (sr = sr/2 - (s+1)/2 - ETH_ALEN - 1; sr > 0; sr--)
+		outw(0, eth_nic_base + TX_FRAME_PORT);
 
 	/* wait for transfer to succeed */
 	for (tmo = currticks()+5*TICKS_PER_SEC;
