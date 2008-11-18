0a8a17e66944da38de3cf3c1bd1d08aef3a2f5ad
Michael Brown <mcb30@etherboot.org>
2008-11-18 19:43:13 +0000

    [i386] Free allocated base memory on exit, if possible
    Code paths that automatically allocate memory from the FBMS at 40:13
    should also free it, if possible.
    
    Freeing this memory will not be possible if either
    
      1. The FBMS has been modified since our allocation, or
    
      2. We have not been able to unhook one or more BIOS interrupt vectors.

--- src/arch/i386/include/biosint.h
+++ src/arch/i386/include/biosint.h
@@ -6,9 +6,22 @@
  *
  */
 
+#include <realmode.h>
+
 struct segoff;
 
-extern int hooked_bios_interrupts;
+/**
+ * Hooked interrupt count
+ *
+ * At exit, after unhooking all possible interrupts, this counter
+ * should be examined.  If it is non-zero, it means that we failed to
+ * unhook at least one interrupt vector, and so must not free up the
+ * memory we are using.  (Note that this also implies that we should
+ * re-hook INT 15 in order to hide ourselves from the memory map).
+ */
+extern uint16_t __text16 ( hooked_bios_interrupts );
+#define hooked_bios_interrupts __use_text16 ( hooked_bios_interrupts )
+
 extern void hook_bios_interrupt ( unsigned int interrupt, unsigned int handler,
 				  struct segoff *chain_vector );
 extern int unhook_bios_interrupt ( unsigned int interrupt,
--- src/arch/i386/interface/pcbios/biosint.c
+++ src/arch/i386/interface/pcbios/biosint.c
@@ -8,17 +8,6 @@
  */
 
 /**
- * Hooked interrupt count
- *
- * At exit, after unhooking all possible interrupts, this counter
- * should be examined.  If it is non-zero, it means that we failed to
- * unhook at least one interrupt vector, and so must not free up the
- * memory we are using.  (Note that this also implies that we should
- * re-hook INT 15 in order to hide ourselves from the memory map).
- */
-int hooked_bios_interrupts = 0;
-
-/**
  * Hook INT vector
  *
  * @v interrupt		INT number
