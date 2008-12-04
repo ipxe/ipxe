3a799e99ca026dd01bb1f69c04cdf2b6f2c6d6da
Michael Brown <mcb30@etherboot.org>
2008-12-04 20:48:53 +0000

    [hermon] Add PCI ID for ConnectX QDR card
    Patch provided by Itay Gazit <itaygazit@gmail.com>.

--- src/drivers/infiniband/hermon.c
+++ src/drivers/infiniband/hermon.c
@@ -2309,6 +2309,7 @@ static struct pci_device_id hermon_nics[] = {
 	PCI_ROM ( 0x15b3, 0x6340, "mt25408", "MT25408 HCA driver" ),
 	PCI_ROM ( 0x15b3, 0x634a, "mt25418", "MT25418 HCA driver" ),
 	PCI_ROM ( 0x15b3, 0x6732, "mt26418", "MT26418 HCA driver" ),
+	PCI_ROM ( 0x15b3, 0x673c, "mt26428", "MT26428 HCA driver" ),
 };
 
 struct pci_driver hermon_driver __pci_driver = {
