ipxe/ipxe

commit b149a99302d54728cf1d505b48ef9ebe99733391

[build] Round up SUBx deltas
The zbin compressor fixup utility rounds down file sizes before
calculating their difference.  This produces incorrect values and may
cause truncated gPXE images to be loaded at boot.

The following example explains the problem:
ilen    = 48 bytes     (uncompressed input file)
olen    = 17 bytes     (compressed output file)
divisor = 16 bytes     (paragraph granularity)
fixmeup = 3 paragraphs (value to fix up)

olen / divisor - ilen / divisor
= 1 - 3
= -2 paragraphs  (old delta calculation)

( align ( olen, divisor ) - align ( ilen, divisor ) ) / divisor
= 2 - 3
= -1 paragraphs  (new delta calculation)

If we perform the SUBx operation with old delta:
fixmeup + -2 = 1 paragraph gets loaded by the prefix

With the new delta:
fixmeup + -1 = 2 paragraphs get loaded by the prefix

The old delta calculation removes the last paragraph; the prefix will
load a truncated copy of gPXE into memory.  We need to load 2
paragraphs since olen is 17 bytes.  Loading only 1 paragraph (16
bytes) would truncate the last byte.

Signed-off-by: Michael Brown <mcb30@etherboot.org>

src/util/zbin.c
@@ -57,6 +57,10 @@ struct zinfo_file {
 	unsigned int num_entries;
 };
 
+static unsigned long align ( unsigned long value, unsigned long align ) {
+	return ( ( value + align - 1 ) & ~( align - 1 ) );
+}
+
 static int read_file ( const char *filename, void **buf, size_t *len ) {
 	FILE *file;
 	struct stat stat;
@@ -140,14 +144,13 @@ static int process_zinfo_copy ( struct input_file *input,
 	struct zinfo_copy *copy = &zinfo->copy;
 	size_t offset = copy->offset;
 	size_t len = copy->len;
-	unsigned int align = copy->align;
 
 	if ( ( offset + len ) > input->len ) {
 		fprintf ( stderr, "Input buffer overrun on copy\n" );
 		return -1;
 	}
 
-	output->len = ( ( output->len + align - 1 ) & ~( align - 1 ) );
+	output->len = align ( output->len, copy->align );
 	if ( ( output->len + len ) > output->max_len ) {
 		fprintf ( stderr, "Output buffer overrun on copy\n" );
 		return -1;
@@ -170,7 +173,6 @@ static int process_zinfo_pack ( struct input_file *input,
 	struct zinfo_pack *pack = &zinfo->pack;
 	size_t offset = pack->offset;
 	size_t len = pack->len;
-	unsigned int align = pack->align;
 	unsigned long packed_len;
 
 	if ( ( offset + len ) > input->len ) {
@@ -178,7 +180,7 @@ static int process_zinfo_pack ( struct input_file *input,
 		return -1;
 	}
 
-	output->len = ( ( output->len + align - 1 ) & ~( align - 1 ) );
+	output->len = align ( output->len, pack->align );
 	if ( output->len > output->max_len ) {
 		fprintf ( stderr, "Output buffer overrun on pack\n" );
 		return -1;
@@ -222,8 +224,9 @@ static int process_zinfo_subtract ( struct input_file *input,
 	}
 
 	target = ( output->buf + offset );
-	delta = ( ( output->len / subtract->divisor ) -
-		  ( input->len / subtract->divisor ) );
+	delta = ( ( align ( output->len, subtract->divisor ) -
+		    align ( input->len, subtract->divisor ) )
+		  / subtract->divisor );
 
 	switch ( datasize ) {
 	case 1: {
@@ -251,7 +254,7 @@ static int process_zinfo_subtract ( struct input_file *input,
 	}
 
 	if ( DEBUG ) {
-		fprintf ( stderr, "SUBx [%#zx,%#zx) (%#lx+(%#lx/%#lx)-(%#lx/%#lx)) = %#lx\n",
+		fprintf ( stderr, "SUBx [%#zx,%#zx) (%#lx+(%#lx/%#x)-(%#lx/%#x)) = %#lx\n",
 			  offset, ( offset + datasize ), old, output->len, subtract->divisor,
 			  input->len, subtract->divisor, new );
 	}

commit 9b964dec363b12f5f18057d740d96e887cf58186

[build] Fix signed/unsigned division in util/zbin.c
Commit b149a99 ([build] Round up SUBx deltas) introduced a
signed/unsigned issue that affects gPXE images built on 32-bit hosts.
The zbin fixup utility performed an unsigned division, which led to
.usb images with an incorrect number of sectors to load.

The issue snuck by on 64-bit hosts since uint32_t is promoted to long.
On 32-bit hosts it is promoted to unsigned long.

Modified-by: Michael Brown <mcb30@etherboot.org>
Signed-off-by: Michael Brown <mcb30@etherboot.org>

src/util/zbin.c
@@ -213,7 +213,8 @@ static int process_zinfo_subtract ( struct input_file *input,
 				    size_t datasize ) {
 	size_t offset = subtract->offset;
 	void *target;
-	long delta;
+	signed long raw_delta;
+	signed long delta;
 	unsigned long old;
 	unsigned long new;
 
@@ -224,9 +225,9 @@ static int process_zinfo_subtract ( struct input_file *input,
 	}
 
 	target = ( output->buf + offset );
-	delta = ( ( align ( output->len, subtract->divisor ) -
-		    align ( input->len, subtract->divisor ) )
-		  / subtract->divisor );
+	raw_delta = ( align ( output->len, subtract->divisor ) -
+		      align ( input->len, subtract->divisor ) );
+	delta = ( raw_delta / ( ( signed long ) subtract->divisor ) );
 
 	switch ( datasize ) {
 	case 1: {