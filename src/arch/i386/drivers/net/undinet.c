198ae0a1313b1dd8297452e761ee8788fb0ba01e
Michael Brown <mcb30@etherboot.org>
2009-06-23 19:26:07 +0100

    [undi] Include PXENV_GET_IFACE_INFO's ServiceFlags in debug output

--- src/arch/i386/drivers/net/undinet.c
+++ src/arch/i386/drivers/net/undinet.c
@@ -705,8 +705,9 @@ int undinet_probe ( struct undi_device *undi ) {
 				   &undi_iface,
 				   sizeof ( undi_iface ) ) ) != 0 )
 		goto err_undi_get_iface_info;
-	DBGC ( undinic, "UNDINIC %p has type %s and link speed %d\n",
-	       undinic, undi_iface.IfaceType, undi_iface.LinkSpeed );
+	DBGC ( undinic, "UNDINIC %p has type %s, speed %d, flags %08x\n",
+	       undinic, undi_iface.IfaceType, undi_iface.LinkSpeed,
+	       undi_iface.ServiceFlags );
 	if ( strncmp ( ( ( char * ) undi_iface.IfaceType ), "Etherboot",
 		       sizeof ( undi_iface.IfaceType ) ) == 0 ) {
 		DBGC ( undinic, "UNDINIC %p Etherboot 5.4 workaround enabled\n",
