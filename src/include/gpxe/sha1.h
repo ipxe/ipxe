59b7d00c068249aba617384dcb2df2441fd878c2
Joshua Oreman <oremanj@rwcr.net>
2009-06-26 00:03:12 -0700

    [digest] Add HMAC-SHA1 based pseudorandom function and PBKDF2
    Both of these routines are used by 802.11 WPA, but they are generic
    and could be needed by other protocols as well.
    
    Signed-off-by: Marty Connor <mdc@etherboot.org>

--- src/include/gpxe/sha1.h
+++ src/include/gpxe/sha1.h
@@ -12,4 +12,13 @@ struct digest_algorithm;
 
 extern struct digest_algorithm sha1_algorithm;
 
+/* SHA1-wrapping functions defined in sha1extra.c: */
+
+void prf_sha1 ( const void *key, size_t key_len, const char *label,
+		const void *data, size_t data_len, void *prf, size_t prf_len );
+
+void pbkdf2_sha1 ( const void *passphrase, size_t pass_len,
+		   const void *salt, size_t salt_len,
+		   int iterations, void *key, size_t key_len );
+
 #endif /* _GPXE_SHA1_H */
