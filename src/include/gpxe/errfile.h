ce64398f87230d14a62b2ff153a67b851a125d2e
Joshua Oreman <oremanj@rwcr.net>
2009-07-21 11:16:38 -0700

    [802.11] Add support for 802.11 devices with software MAC layer
    This is required for all modern 802.11 devices, and allows drivers
    to be written for them with minimally more effort than is required
    for a wired NIC.
    
    Signed-off-by: Michael Brown <mcb30@etherboot.org>
    Modified-by: Michael Brown <mcb30@etherboot.org>

--- src/include/gpxe/errfile.h
+++ src/include/gpxe/errfile.h
@@ -148,6 +148,7 @@ FILE_LICENCE ( GPL2_OR_LATER );
 #define ERRFILE_ib_pathrec		( ERRFILE_NET | 0x001c0000 )
 #define ERRFILE_ib_mcast		( ERRFILE_NET | 0x001d0000 )
 #define ERRFILE_ib_cm			( ERRFILE_NET | 0x001e0000 )
+#define ERRFILE_net80211		( ERRFILE_NET | 0x001f0000 )
 
 #define ERRFILE_image		      ( ERRFILE_IMAGE | 0x00000000 )
 #define ERRFILE_elf		      ( ERRFILE_IMAGE | 0x00010000 )
