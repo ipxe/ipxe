e84db1121b206736eb2958322abd0830b068132d
Piotr Jaroszyński <p.jaroszynski@gmail.com>
2010-04-08 01:25:38 +0200

    [linux] Add linux platform skeleton
    Add makefiles, ld scripts and default config for linux platform for
    both i386 and x86_64.
    
    Signed-off-by: Piotr Jaroszyński <p.jaroszynski@gmail.com>
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

---
+++ src/config/defaults/linux.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_DEFAULTS_LINUX_H
+#define CONFIG_DEFAULTS_LINUX_H
+
+/** @file
+ *
+ * Configuration defaults for linux
+ *
+ */
+
+#define IMAGE_SCRIPT
+
+#endif /* CONFIG_DEFAULTS_LINUX_H */
