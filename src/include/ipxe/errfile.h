bc83e4b5d3bd408cdab252715f7e269db174ecbe
Michael Brown <mcb30@ipxe.org>
2010-05-28 14:58:04 +0100

    [base16] Add generic base16 encoding and decoding routines
    Base16 encoding is currently implemented in both iSCSI and SRP.
    
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/include/ipxe/errfile.h
+++ src/include/ipxe/errfile.h
@@ -54,6 +54,7 @@ FILE_LICENCE ( GPL2_OR_LATER );
 #define ERRFILE_xfer		       ( ERRFILE_CORE | 0x000e0000 )
 #define ERRFILE_bitmap		       ( ERRFILE_CORE | 0x000f0000 )
 #define ERRFILE_base64		       ( ERRFILE_CORE | 0x00100000 )
+#define ERRFILE_base16		       ( ERRFILE_CORE | 0x00110000 )
 
 #define ERRFILE_eisa		     ( ERRFILE_DRIVER | 0x00000000 )
 #define ERRFILE_isa		     ( ERRFILE_DRIVER | 0x00010000 )
