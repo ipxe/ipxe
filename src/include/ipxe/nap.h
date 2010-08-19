pjaroszynski/gpxe

[linux] Add nap
Signed-off-by: Piotr Jaroszyński <p.jaroszynski@gmail.com>

src/include/gpxe/nap.h
@@ -43,6 +43,7 @@ FILE_LICENCE ( GPL2_OR_LATER );
 
 /* Include all architecture-independent I/O API headers */
 #include <gpxe/null_nap.h>
+#include <gpxe/linux/linux_nap.h>
 
 /* Include all architecture-dependent I/O API headers */
 #include <bits/nap.h>

ipxe/ipxe

Signed-off-by: Michael Brown <mcb30@ipxe.org>

src/include/ipxe/nap.h
@@ -43,6 +43,7 @@ FILE_LICENCE ( GPL2_OR_LATER );
 
 /* Include all architecture-independent I/O API headers */
 #include <ipxe/null_nap.h>
+#include <ipxe/linux/linux_nap.h>
 
 /* Include all architecture-dependent I/O API headers */
 #include <bits/nap.h>