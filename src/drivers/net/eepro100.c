b0eacbd42166982e675e3467393a2e63fdb99111
Marty Connor <mdc@etherboot.org>
2010-08-30 10:24:51 -0400

    [eepro100] Add new PCI ID 8086:27dc
    Add PCI ID 8086:27dc to the eepro100 driver.
    
    Reported-by: Cédric Delmas <c.delmas@akka.eu>
    Signed-off-by: Thomas Miletich <thomas.miletich@gmail.com>
    Signed-off-by: Marty Connor <mdc@etherboot.org>
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/drivers/net/eepro100.c
+++ src/drivers/net/eepro100.c
@@ -1141,6 +1141,7 @@ PCI_ROM(0x8086, 0x1065, "82562-3",       "Intel 82562 based Fast Ethernet Connec
 PCI_ROM(0x8086, 0x5200, "eepro100-5200", "Intel EtherExpress PRO/100 Intelligent Server", 0),
 PCI_ROM(0x8086, 0x5201, "eepro100-5201", "Intel EtherExpress PRO/100 Intelligent Server", 0),
 PCI_ROM(0x8086, 0x1092, "82562-3",       "Intel Pro/100 VE Network", 0),
+PCI_ROM(0x8086, 0x27dc, "eepro100-27dc", "Intel 82801G (ICH7) Chipset Ethernet Controller", 0),
 };
 
 /* Cards with device ids 0x1030 to 0x103F, 0x2449, 0x2459 or 0x245D might need
