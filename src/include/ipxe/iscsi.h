ipxe/ipxe

commit 5f4ab0d22ab385ca5ba4d5783a6557676c60e417

[iscsi] Randomise a portion of the ISID to force new session instantiation
When a connection to an iSCSI target is broken without gracefully
closing the TCP socket, a subsequent connection attempt may fail
because the target believes that we are attempting session
reinstatement (see RFC3720 section 5.3.1).  This has been observed
using the Microsoft iSCSI target.

Section 9.1.1 of RFC3720 states that initiators should use a stable
ISID, however section 5.3.1 shows that the only way to explicitly
request that a new session be created is to use a new ISID.

Fix by randomising the "qualifier" portion of the ISID.

Signed-off-by: Michael Brown <mcb30@ipxe.org>

src/net/tcp/iscsi.c
@@ -707,7 +707,7 @@ static void iscsi_start_login ( struct iscsi_session *iscsi ) {
 	ISCSI_SET_LENGTHS ( request->lengths, 0, len );
 	request->isid_iana_en = htonl ( ISCSI_ISID_IANA |
 					IANA_EN_FEN_SYSTEMS );
-	/* isid_iana_qual left as zero */
+	request->isid_iana_qual = ( random() & 0xffff );
 	/* tsih left as zero */
 	request->itt = htonl ( iscsi->itt );
 	/* cid left as zero */

commit 19c59bb1317e58c44ce606dadbd0ee8dcfcf19ac

[iscsi] Ensure ISID is consistent within an iSCSI session
Commit 5f4ab0d ("[iscsi] Randomise a portion of the ISID to force new
session instantiation") introduced a regression by randomising the
ISID on each call to iscsi_start_login(), which may be called more
than once per connection, rather than on each call to
iscsi_open_connection(), which is guaranteed to be called only once
per connection.  This is incorrect behaviour that causes our
connection to be rejected by some iSCSI targets (observed with a
COMSTAR target under OpenSolaris).

Fix by generating the ISID in iscsi_open_connection(), and storing the
randomised ISID as part of the session state.

Signed-off-by: Michael Brown <mcb30@ipxe.org>

src/include/ipxe/iscsi.h
@@ -539,6 +539,12 @@ struct iscsi_session {
 	/** CHAP response (used for both initiator and target auth) */
 	struct chap_response chap;
 
+	/** Initiator session ID (IANA format) qualifier
+	 *
+	 * This is part of the ISID.  It is generated randomly
+	 * whenever a new connection is opened.
+	 */
+	uint16_t isid_iana_qual;
 	/** Initiator task tag
 	 *
 	 * This is the tag of the current command.  It is incremented

src/net/tcp/iscsi.c
@@ -246,6 +246,9 @@ static int iscsi_open_connection ( struct iscsi_session *iscsi ) {
 	if ( iscsi->target_username )
 		iscsi->status |= ISCSI_STATUS_AUTH_REVERSE_REQUIRED;
 
+	/* Assign new ISID */
+	iscsi->isid_iana_qual = ( random() & 0xffff );
+
 	/* Assign fresh initiator task tag */
 	iscsi_new_itt ( iscsi );
 
@@ -707,7 +710,7 @@ static void iscsi_start_login ( struct iscsi_session *iscsi ) {
 	ISCSI_SET_LENGTHS ( request->lengths, 0, len );
 	request->isid_iana_en = htonl ( ISCSI_ISID_IANA |
 					IANA_EN_FEN_SYSTEMS );
-	request->isid_iana_qual = ( random() & 0xffff );
+	request->isid_iana_qual = htons ( iscsi->isid_iana_qual );
 	/* tsih left as zero */
 	request->itt = htonl ( iscsi->itt );
 	/* cid left as zero */