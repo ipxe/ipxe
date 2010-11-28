795793b3a4751936db4dc331aeb5f5549cfb3987
Michael Brown <mcb30@ipxe.org>
2010-11-28 17:26:27 +0000

    [build] Use __builtin_offsetof() when available
    Some newer versions of gcc (observed with a patched gcc 4.5.1) seem to
    treat our offsetof() implementation as not being a compile-time
    constant.  Fix by using __builtin_offsetof() when available.  (As with
    the original offsetof() macro, this code is copied from the Linux
    kernel's stddef.h.)
    
    Reported-by: Arkadiusz Miskiewicz <arekm@maven.pl>
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/include/stddef.h
+++ src/include/stddef.h
@@ -10,7 +10,11 @@ FILE_LICENCE ( GPL2_ONLY );
 #define NULL ((void *)0)
 
 #undef offsetof
+#ifdef __compiler_offsetof
+#define offsetof(TYPE,MEMBER) __compiler_offsetof(TYPE,MEMBER)
+#else
 #define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#endif
 
 #undef container_of
 #define container_of(ptr, type, member) ({                      \
