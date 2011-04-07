af5e74990cb1d5967bb9d51dde964010b0172e1f
Michael Brown <mcb30@ipxe.org>
2011-04-07 22:58:37 +0100

    [efi] Include MdeModulePkg directory in header import list
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/include/ipxe/efi/import.pl
+++ src/include/ipxe/efi/import.pl
@@ -112,7 +112,8 @@ pod2usage ( 1 ) unless @ARGV == 1;
 my $edktop = shift;
 
 # Identify edk import directories
-my $edkdirs = [ "MdePkg/Include", "IntelFrameworkPkg/Include" ];
+my $edkdirs = [ "MdePkg/Include", "IntelFrameworkPkg/Include",
+		"MdeModulePkg/Include" ];
 foreach my $edkdir ( @$edkdirs ) {
   die "Directory \"$edktop\" does not appear to contain the EFI EDK2 "
       ."(missing \"$edkdir\")\n" unless -d catdir ( $edktop, $edkdir );
