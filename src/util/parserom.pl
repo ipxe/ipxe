ipxe/ipxe

commit 9b99d2af0cdcedcaaf9a69a0c758b8a7e644236a

[build] Avoid generating ROMs with "match-any" vendor or device IDs
A PCI_ROM() entry containing a vendor or device ID of PCI_ANY_ID
(0xffff) indicates to pci_find_driver() that the entry's vendor or
device ID should be ignored when matching against the device's vendor
or device ID.  It does not represent a PCI ROM that should be built.

Signed-off-by: Michael Brown <mcb30@ipxe.org>

src/util/parserom.pl
@@ -49,6 +49,7 @@ while ( <DRV> ) {
          \s*.*\s*		   # Driver data
        \)/x ) {
     ( my $vendor, my $device, my $image, my $desc ) = ( lc $1, lc $2, $3, $4 );
+    next if ( $vendor eq "ffff" ) || ( $device eq "ffff" );
     rom ( "pci", lc "${vendor}${device}", $desc, $vendor, $device );
     rom ( "pci", $image, $desc, $vendor, $device, 1 );
   } elsif ( /^\s*ISA_ROM\s*\(

commit c46acda67200bf01c27d1d039f1abc9b0e71b2b7

[build] Include UNDI PCI driver within all-drivers build
Commit 9b99d2a ("[build] Avoid generating ROMs with "match-any" vendor
or device IDs") introduced a regression which caused the UNDI PCI
driver to be omitted from the list of all drivers, and thus to be
excluded from the all-drivers build.

Fix by ensuring that the per-driver section of the Makefile is
generated even when there are no ROMs to be built.

Reported-by: Sven Dreyer <sven@dreyer-net.de>
Signed-off-by: Michael Brown <mcb30@ipxe.org>

src/util/parserom.pl
@@ -28,6 +28,7 @@ sub rom {
     $printed_family = 1;
   }
   print "\n";
+  return if ( $vendor && ( ( $vendor eq "ffff" ) || ( $device eq "ffff" ) ) );
   print "# NIC\t$image\t$ids\t$desc\n";
   print "DRIVER_$image = $driver_name\n";
   print "ROM_TYPE_$image = $type\n";
@@ -49,7 +50,6 @@ while ( <DRV> ) {
          \s*.*\s*		   # Driver data
        \)/x ) {
     ( my $vendor, my $device, my $image, my $desc ) = ( lc $1, lc $2, $3, $4 );
-    next if ( $vendor eq "ffff" ) || ( $device eq "ffff" );
     rom ( "pci", lc "${vendor}${device}", $desc, $vendor, $device );
     rom ( "pci", $image, $desc, $vendor, $device, 1 );
   } elsif ( /^\s*ISA_ROM\s*\(