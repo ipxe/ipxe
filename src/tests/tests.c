1f238bc69e1aa56f79e163e60fd7c733ef7e0eb8
Michael Brown <mcb30@ipxe.org>
2012-03-06 21:01:30 +0000

    [test] Add NIST self-tests for AES128 and AES256 in CBC mode
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/tests/tests.c
+++ src/tests/tests.c
@@ -30,5 +30,6 @@ REQUIRE_OBJECT ( byteswap_test );
 REQUIRE_OBJECT ( md5_test );
 REQUIRE_OBJECT ( sha1_test );
 REQUIRE_OBJECT ( sha256_test );
+REQUIRE_OBJECT ( aes_cbc_test );
 REQUIRE_OBJECT ( hmac_drbg_test );
 REQUIRE_OBJECT ( hash_df_test );
