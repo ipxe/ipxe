945e4281375f86da5e59f4c9447edae7af7850c1
Michael Brown <mcb30@ipxe.org>
2012-04-23 23:29:18 +0100

    [intel] Replace driver for Intel Gigabit NICs
    Tested-by: Robin Smidsrød <robin@smidsrod.no>
    Tested-by: Thomas Miletich <thomas.miletich@gmail.com>
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/include/ipxe/errfile.h
+++ src/include/ipxe/errfile.h
@@ -144,6 +144,7 @@ FILE_LICENCE ( GPL2_OR_LATER );
 #define ERRFILE_mii		     ( ERRFILE_DRIVER | 0x00620000 )
 #define ERRFILE_realtek		     ( ERRFILE_DRIVER | 0x00630000 )
 #define ERRFILE_skeleton	     ( ERRFILE_DRIVER | 0x00640000 )
+#define ERRFILE_intel		     ( ERRFILE_DRIVER | 0x00650000 )
 
 #define ERRFILE_scsi		     ( ERRFILE_DRIVER | 0x00700000 )
 #define ERRFILE_arbel		     ( ERRFILE_DRIVER | 0x00710000 )
