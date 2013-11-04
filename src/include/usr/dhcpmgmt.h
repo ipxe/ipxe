c99f9ee9dfbff6dad087693ae253cebb1776ec03
Michael Brown <mcb30@ipxe.org>
2013-11-04 23:07:06 +0000

    [dhcp] Remove obsolete dhcp() function
    All functionality provided by dhcp() has now been obviated by the more
    generic ifconf().
    
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/include/usr/dhcpmgmt.h
+++ src/include/usr/dhcpmgmt.h
@@ -11,7 +11,6 @@ FILE_LICENCE ( GPL2_OR_LATER );
 
 struct net_device;
 
-extern int dhcp ( struct net_device *netdev );
 extern int pxebs ( struct net_device *netdev, unsigned int pxe_type );
 
 #endif /* _USR_DHCPMGMT_H */
--- src/usr/dhcpmgmt.c
+++ src/usr/dhcpmgmt.c
@@ -25,38 +25,14 @@ FILE_LICENCE ( GPL2_OR_LATER );
 #include <ipxe/netdevice.h>
 #include <ipxe/dhcp.h>
 #include <ipxe/monojob.h>
-#include <ipxe/timer.h>
 #include <usr/ifmgmt.h>
 #include <usr/dhcpmgmt.h>
-
-/** Default time to wait for link-up */
-#define LINK_WAIT_TIMEOUT ( 15 * TICKS_PER_SEC )
 
 /** @file
  *
  * DHCP management
  *
  */
-
-int dhcp ( struct net_device *netdev ) {
-	int rc;
-
-	/* Check we can open the interface first */
-	if ( ( rc = ifopen ( netdev ) ) != 0 )
-		return rc;
-
-	/* Wait for link-up */
-	if ( ( rc = iflinkwait ( netdev, LINK_WAIT_TIMEOUT ) ) != 0 )
-		return rc;
-
-	/* Perform DHCP */
-	printf ( "DHCP (%s %s)", netdev->name,
-		 netdev->ll_protocol->ntoa ( netdev->ll_addr ) );
-	if ( ( rc = start_dhcp ( &monojob, netdev ) ) == 0 )
-		rc = monojob_wait ( "", 0 );
-
-	return rc;
-}
 
 int pxebs ( struct net_device *netdev, unsigned int pxe_type ) {
 	int rc;
