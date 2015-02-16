b54167b8b6a35c7dab19bbe4b421d447036558d4
Michael Brown <mcb30@ipxe.org>
2015-02-16 15:33:32 +0000

    [libc] Remove unused string functions
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/include/string.h
+++ src/include/string.h
@@ -19,11 +19,9 @@ FILE_LICENCE ( GPL2_ONLY );
 #include <stddef.h>
 #include <bits/string.h>
 
-int __pure strnicmp(const char *s1, const char *s2, size_t len) __nonnull;
 char * strcpy(char * dest,const char *src) __nonnull;
 char * strncpy(char * dest,const char *src,size_t count) __nonnull;
 char * strcat(char * dest, const char * src) __nonnull;
-char * strncat(char *dest, const char *src, size_t count) __nonnull;
 int __pure strcmp(const char * cs,const char * ct) __nonnull;
 int __pure strncmp(const char * cs,const char * ct,
 				     size_t count) __nonnull;
@@ -31,17 +29,13 @@ char * __pure strchr(const char * s, int c) __nonnull;
 char * __pure strrchr(const char * s, int c) __nonnull;
 size_t __pure strlen(const char * s) __nonnull;
 size_t __pure strnlen(const char * s, size_t count) __nonnull;
-size_t __pure strspn(const char *s, const char *accept) __nonnull;
-size_t __pure strcspn(const char *s, const char *reject) __nonnull;
 char * __pure strpbrk(const char * cs,const char * ct) __nonnull;
-char * strtok(char * s,const char * ct) __nonnull;
 char * strsep(char **s, const char *ct) __nonnull;
 void * memset(void * s,int c,size_t count) __nonnull;
 void * memcpy ( void *dest, const void *src, size_t len ) __nonnull;
 void * memmove(void * dest,const void *src,size_t count) __nonnull;
 int __pure memcmp(const void * cs,const void * ct,
 				    size_t count) __nonnull;
-void * __pure memscan(const void * addr, int c, size_t size) __nonnull;
 char * __pure strstr(const char * s1,const char * s2) __nonnull;
 void * __pure memchr(const void *s, int c, size_t n) __nonnull;
 char * __malloc strdup(const char *s) __nonnull;
