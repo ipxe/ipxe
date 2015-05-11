a8e4187c455aa89f2de7376f959a24f6c7327e11
Michael Brown <mcb30@ipxe.org>
2015-05-11 14:57:28 +0100

    [usb] Add generic USB human interface device (HID) framework
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/include/ipxe/errfile.h
+++ src/include/ipxe/errfile.h
@@ -81,6 +81,7 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #define ERRFILE_xhci		     ( ERRFILE_DRIVER | 0x00090000 )
 #define ERRFILE_ehci		     ( ERRFILE_DRIVER | 0x000a0000 )
 #define ERRFILE_uhci		     ( ERRFILE_DRIVER | 0x000b0000 )
+#define ERRFILE_usbhid		     ( ERRFILE_DRIVER | 0x000c0000 )
 
 #define ERRFILE_nvs		     ( ERRFILE_DRIVER | 0x00100000 )
 #define ERRFILE_spi		     ( ERRFILE_DRIVER | 0x00110000 )
