ipxe/ipxe

commit dc19e630cb44b8ee313ac4b3852d92fcdf050dfc

[build] Construct all-drivers list based on driver class
The USB bus drivers (ehci.c and xhci.c) have PCI device ID tables and
hence PCI_ROM() lines, but should probably not be included in the
all-drivers build on this basis, since they do nothing useful unless a
USB network driver is also present.

Fix by constructing the all-drivers list based on the driver class
(i.e. the portion of the source path immediately after "drivers/").

Signed-off-by: Michael Brown <mcb30@ipxe.org>

src/Makefile.housekeeping
@@ -915,6 +915,8 @@ BOBJS	+= $(patsubst %,$(BIN)/%.ids.o,$(DRIVERS))
 #
 bobjs :
 	@$(ECHO) $(BOBJS)
+drivers_% :
+	@$(ECHO) $(DRIVERS_$*)
 drivers :
 	@$(ECHO) $(DRIVERS)
 .PHONY : drivers
@@ -945,6 +947,12 @@ $(BIN)/NIC : $(AUTO_DEPS)
 	@perl -ne 'chomp; print "$$1\n" if /\# NIC\t(.*)$$/' $^ >> $@
 CLEANUP		+= $(BIN)/NIC	# Doesn't match the $(BIN)/*.* pattern
 
+# Select drivers to be included in the all-drivers build
+#
+DRIVERS		= $(DRIVERS_net) $(DRIVERS_infiniband) \
+		  $(DRIVERS_xen) $(DRIVERS_hyperv)
+DRIVERS_ipxe	= $(DRIVERS)
+
 # Analyse a target name (e.g. "bin/dfe538--prism2_pci.rom.tmp") and
 # derive the variables:
 # 
@@ -953,7 +961,6 @@ CLEANUP		+= $(BIN)/NIC	# Doesn't match the $(BIN)/*.* pattern
 # TGT_DRIVERS  : the driver for each element (e.g. "rtl8139 prism2_pci")
 # TGT_ROM_NAME : the ROM name (e.g. "dfe538")
 #
-DRIVERS_ipxe	= $(DRIVERS)
 CARD_DRIVER	= $(firstword $(DRIVER_$(1)) $(1))
 TGT_ELEMENTS	= $(subst --, ,$(firstword $(subst ., ,$(notdir $@))))
 TGT_ROM_NAME	= $(firstword $(TGT_ELEMENTS))

src/util/parserom.pl
@@ -161,7 +161,7 @@ sub print_make_rules {
     unless ( $state->{'is_header_printed'} ) {
         print "# NIC\t\n";
         print "# NIC\tfamily\t$state->{family}\n";
-        print "DRIVERS += $state->{driver_name}\n";
+        print "DRIVERS_$state->{driver_class} += $state->{driver_name}\n";
         print "\n";
         $state->{'is_header_printed'} = 1;
     }

src/arch/x86/Makefile
@@ -19,4 +19,4 @@ CFLAGS		+= -DNVALGRIND
 
 # Include Hyper-V driver in the all-drivers build
 #
-DRIVERS += hyperv
+DRIVERS_hyperv += hyperv

src/arch/x86/Makefile.efi
@@ -17,7 +17,7 @@ NON_AUTO_MEDIA	+= efirom
 
 # Include SNP driver in the all-drivers build
 #
-DRIVERS += snp
+DRIVERS_net += snp
 
 # Rules for building EFI files
 #

commit 25d6c80498b98a9b41ca1fec50cd2038f6f17547

[build] Fix .ids.o creation for drivers not in the all-drivers build
Commit dc19e63 ("[build] Construct all-drivers list based on driver
class") accidentally excluded the USB bus drivers from the list of
files parsed in order to create PCI 3.0 device ID lists.

Fix by returning $(DRIVERS) to its previous definition as a list of
all driver files, and use only $(DRIVERS_ipxe) to contain the
filtered list containing only those drivers which we want to include
in the "all-drivers" build.

Reported-by: Mary-Ann Johnson <MaryAnn.Johnson@displaylink.com>
Signed-off-by: Michael Brown <mcb30@ipxe.org>

src/Makefile.housekeeping
@@ -949,9 +949,8 @@ CLEANUP		+= $(BIN)/NIC	# Doesn't match the $(BIN)/*.* pattern
 
 # Select drivers to be included in the all-drivers build
 #
-DRIVERS		= $(DRIVERS_net) $(DRIVERS_infiniband) \
+DRIVERS_ipxe	= $(DRIVERS_net) $(DRIVERS_infiniband) \
 		  $(DRIVERS_xen) $(DRIVERS_hyperv)
-DRIVERS_ipxe	= $(DRIVERS)
 
 # Analyse a target name (e.g. "bin/dfe538--prism2_pci.rom.tmp") and
 # derive the variables:

src/util/parserom.pl
@@ -162,6 +162,7 @@ sub print_make_rules {
         print "# NIC\t\n";
         print "# NIC\tfamily\t$state->{family}\n";
         print "DRIVERS_$state->{driver_class} += $state->{driver_name}\n";
+        print "DRIVERS += $state->{driver_name}\n";
         print "\n";
         $state->{'is_header_printed'} = 1;
     }