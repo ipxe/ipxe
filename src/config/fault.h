dc9d24e7d29ae6926fcafadbc8ca4089f0aae616
Michael Brown <mcb30@ipxe.org>
2015-08-17 13:16:33 +0100

    [peerdist] Add segment discovery mechanism
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/config/fault.h
+++ src/config/fault.h
@@ -14,6 +14,9 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 /* Drop every N transmitted or received network packets */
 #define	NETDEV_DISCARD_RATE 0
 
+/* Drop every N transmitted or received PeerDist discovery packets */
+#define PEERDISC_DISCARD_RATE 0
+
 #include <config/local/fault.h>
 
 #endif /* CONFIG_FAULT_H */
--- src/include/ipxe/errfile.h
+++ src/include/ipxe/errfile.h
@@ -252,6 +252,7 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #define ERRFILE_httpauth		( ERRFILE_NET | 0x00420000 )
 #define ERRFILE_httpbasic		( ERRFILE_NET | 0x00430000 )
 #define ERRFILE_httpdigest		( ERRFILE_NET | 0x00440000 )
+#define ERRFILE_peerdisc		( ERRFILE_NET | 0x00450000 )
 
 #define ERRFILE_image		      ( ERRFILE_IMAGE | 0x00000000 )
 #define ERRFILE_elf		      ( ERRFILE_IMAGE | 0x00010000 )
