0588c03772d1fbbeca66362363ec866d745f95ed
Mika Tiainen <mikat@iki.fi>
2016-02-09 22:38:29 +0200

    [intel] Add INTEL_NO_PHY_RST for another I218-LM variant
    Fixed booting on HP EliteBook 820 G2 laptop.
    
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/drivers/net/intel.c
+++ src/drivers/net/intel.c
@@ -1065,7 +1065,7 @@ static struct pci_device_id intel_nics[] = {
 	PCI_ROM ( 0x8086, 0x157b, "i210-2", "I210", 0 ),
 	PCI_ROM ( 0x8086, 0x15a0, "i218lm-2", "I218-LM", INTEL_NO_PHY_RST ),
 	PCI_ROM ( 0x8086, 0x15a1, "i218v-2", "I218-V", 0 ),
-	PCI_ROM ( 0x8086, 0x15a2, "i218lm-3", "I218-LM", 0 ),
+	PCI_ROM ( 0x8086, 0x15a2, "i218lm-3", "I218-LM", INTEL_NO_PHY_RST ),
 	PCI_ROM ( 0x8086, 0x15a3, "i218v-3", "I218-V", INTEL_NO_PHY_RST ),
 	PCI_ROM ( 0x8086, 0x294c, "82566dc-2", "82566DC-2", 0 ),
 	PCI_ROM ( 0x8086, 0x2e6e, "cemedia", "CE Media Processor", 0 ),
