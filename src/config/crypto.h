f8e1678b84fc3119ce515e0c1a84881bb9ce1c36
Michael Brown <mcb30@ipxe.org>
2016-03-24 19:25:03 +0000

    [crypto] Allow cross-certificate source to be configured at build time
    Provide a build option CROSSCERT in config/crypto.h to allow the
    default cross-signed certificate source to be configured at build
    time.  The ${crosscert} setting may still be used to reconfigure the
    cross-signed certificate source at runtime.
    
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/config/crypto.h
+++ src/config/crypto.h
@@ -50,6 +50,14 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  */
 #define TIMESTAMP_ERROR_MARGIN ( ( 12 * 60 + 30 ) * 60 )
 
+/** Default cross-signed certificate source
+ *
+ * This is the default location from which iPXE will attempt to
+ * download cross-signed certificates in order to complete a
+ * certificate chain.
+ */
+#define CROSSCERT "http://ca.ipxe.org/auto"
+
 #include <config/named.h>
 #include NAMED_CONFIG(crypto.h)
 #include <config/local/crypto.h>
