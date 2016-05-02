ipxe/ipxe

commit 86f96a40f490b1f10662a2d94a86a01276d2a125

[tg3] Remove x86-specific inline assembly
Signed-off-by: Michael Brown <mcb30@ipxe.org>

src/drivers/net/tg3/tg3.h
@@ -3320,43 +3320,25 @@ void tg3_write_indirect_mbox(struct tg3 *tp, u32 off, u32 val);
 
 /* Functions & macros to verify TG3_FLAGS types */
 
-static inline int variable_test_bit(int nr, volatile const unsigned long *addr)
-{
-	int oldbit;
-
-	asm volatile("bt %2,%1\n\t"
-	             "sbb %0,%0"
-	             : "=r" (oldbit)
-	             : "m" (*(unsigned long *)addr), "Ir" (nr));
-
-	return oldbit;
-}
-
 static inline int _tg3_flag(enum TG3_FLAGS flag, unsigned long *bits)
 {
-	return variable_test_bit(flag, bits);
-}
-
-#define BITOP_ADDR(x) "+m" (*(volatile long *) (x))
-
-static inline void __set_bit(int nr, volatile unsigned long *addr)
-{
-	asm volatile("bts %1,%0" : BITOP_ADDR(addr) : "Ir" (nr) : "memory");
+	unsigned int index = ( flag / ( 8 * sizeof ( *bits ) ) );
+	unsigned int bit = ( flag % ( 8 * sizeof ( *bits ) ) );
+	return ( bits[index] & ( 1UL << bit ) );
 }
 
 static inline void _tg3_flag_set(enum TG3_FLAGS flag, unsigned long *bits)
 {
-	__set_bit(flag, bits);
-}
-
-static inline void __clear_bit(int nr, volatile unsigned long *addr)
-{
-	asm volatile("btr %1,%0" : BITOP_ADDR(addr) : "Ir" (nr));
+	unsigned int index = ( flag / ( 8 * sizeof ( *bits ) ) );
+	unsigned int bit = ( flag % ( 8 * sizeof ( *bits ) ) );
+	bits[index] |= ( 1UL << bit );
 }
 
 static inline void _tg3_flag_clear(enum TG3_FLAGS flag, unsigned long *bits)
 {
-	__clear_bit(flag, bits);
+	unsigned int index = ( flag / ( 8 * sizeof ( *bits ) ) );
+	unsigned int bit = ( flag % ( 8 * sizeof ( *bits ) ) );
+	bits[index] &= ~( 1UL << bit );
 }
 
 #define tg3_flag(tp, flag)				\

commit fe62f3c8312c3747c1a6c375b502cffa65d60f63

[tg3] Fix _tg3_flag() for 64-bit builds
Commit 86f96a4 ("[tg3] Remove x86-specific inline assembly")
introduced a regression in _tg3_flag() in 64-bit builds, since any
flags in the upper 32 bits of a 64-bit unsigned long would be
discarded when truncating to a 32-bit int.

Debugged-by: Shane Thompson <shane.thompson@aeontech.com.au>
Tested-by: Shane Thompson <shane.thompson@aeontech.com.au>
Signed-off-by: Michael Brown <mcb30@ipxe.org>

src/drivers/net/tg3/tg3.h
@@ -3324,7 +3324,7 @@ static inline int _tg3_flag(enum TG3_FLAGS flag, unsigned long *bits)
 {
 	unsigned int index = ( flag / ( 8 * sizeof ( *bits ) ) );
 	unsigned int bit = ( flag % ( 8 * sizeof ( *bits ) ) );
-	return ( bits[index] & ( 1UL << bit ) );
+	return ( !! ( bits[index] & ( 1UL << bit ) ) );
 }
 
 static inline void _tg3_flag_set(enum TG3_FLAGS flag, unsigned long *bits)