f6e8b800be5d8656aa2b10827cf7ea93cfcf5048
Vinson Lee <vlee@freedesktop.org>
2016-06-03 18:09:54 +0100

    [build] Remove nested "my" declaration
    Fix build error with perl >= 5.23.2:
    
      Can't redeclare "my" in "my" at ./util/parserom.pl line 160
    
    Signed-off-by: Vinson Lee <vlee@freedesktop.org>
    Reviewed-by: Robin Smidsrød <robin@smidsrod.no>
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/util/parserom.pl
+++ src/util/parserom.pl
@@ -157,7 +157,7 @@ sub process_isa_rom {
 
 # Output Makefile rules for the specified ROM declarations
 sub print_make_rules {
-    my ( $state, my $image, my $desc, my $vendor, my $device, my $dup ) = @_;
+    my ( $state, $image, $desc, $vendor, $device, $dup ) = @_;
     unless ( $state->{'is_header_printed'} ) {
         print "# NIC\t\n";
         print "# NIC\tfamily\t$state->{family}\n";
