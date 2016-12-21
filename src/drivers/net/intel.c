0bfe9f53426242e2b932ed51ed694f5e77fead9a
Christian Nilsson <nikize@gmail.com>
2016-12-21 20:21:33 +0100

    [intel] Add INTEL_NO_PHY_RST for I219-LM (2)
    Originally-implemented-by: Malte zu Klampen <malte@pclab.ifg.uni-kiel.de>
    Originally-implemented-by: Richard Moore <rich@richud.com>
    Tested-by: Esben Storgaard Nielsen <esn@solar.dk>
    Signed-off-by: Christian Nilsson <nikize@gmail.com>
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/drivers/net/intel.c
+++ src/drivers/net/intel.c
@@ -1069,7 +1069,7 @@ static struct pci_device_id intel_nics[] = {
 	PCI_ROM ( 0x8086, 0x15a1, "i218v-2", "I218-V", 0 ),
 	PCI_ROM ( 0x8086, 0x15a2, "i218lm-3", "I218-LM", INTEL_NO_PHY_RST ),
 	PCI_ROM ( 0x8086, 0x15a3, "i218v-3", "I218-V", INTEL_NO_PHY_RST ),
-	PCI_ROM ( 0x8086, 0x15b7, "i219lm-2", "I219-LM (2)", 0 ),
+	PCI_ROM ( 0x8086, 0x15b7, "i219lm-2", "I219-LM (2)", INTEL_NO_PHY_RST ),
 	PCI_ROM ( 0x8086, 0x15b8, "i219v-2", "I219-V (2)", 0 ),
 	PCI_ROM ( 0x8086, 0x294c, "82566dc-2", "82566DC-2", 0 ),
 	PCI_ROM ( 0x8086, 0x2e6e, "cemedia", "CE Media Processor", 0 ),
