2eef77ecc0b3a30a2244961564c17c15920424f6
Rob Taglang <rob@privatemachines.com>
2018-04-05 14:47:23 -0400

    [intelx] Add PCI_ROM entry for Intel X553 NIC
    Modified-by: Michael Brown <mcb30@ipxe.org>
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/drivers/net/intelx.c
+++ src/drivers/net/intelx.c
@@ -474,6 +474,7 @@ static struct pci_device_id intelx_nics[] = {
 	PCI_ROM ( 0x8086, 0x1557, "82599en-sfp", "82599 (Single Port SFI Only)", 0 ),
 	PCI_ROM ( 0x8086, 0x1560, "x540t1", "X540-AT2/X540-BT2 (with single port NVM)", 0 ),
 	PCI_ROM ( 0x8086, 0x1563, "x550t2", "X550-T2", 0 ),
+	PCI_ROM ( 0x8086, 0x15e5, "x553", "X553", 0 ),
 };
 
 /** PCI driver */
