a5c41483d226a6c4dbfaf35f57dc092cd57bc601
Michael Brown <mcb30@ipxe.org>
2019-08-17 17:18:54 +0100

    [coverity] Override assumptions about wcrtomb() and hmac_init()
    Newer versions of Coverity use built-in models for wcrtomb() and
    hmac_init() that are capable of returning errors, and reports defects
    due to code failing to check for these errors.  The actual iPXE
    implementations are simpler than Coverity's models and can never
    return errors, so these defects are false positives.
    
    Fix by overriding Coverity's built-in models for these functions.
    
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- contrib/coverity/model.c
+++ contrib/coverity/model.c
@@ -7,6 +7,9 @@ typedef long off_t;
 typedef void * userptr_t;
 typedef long long time_t;
 struct tm;
+typedef unsigned short wchar_t;
+typedef void mbstate_t;
+struct digest_algorithm;
 
 /* Inhibit use of built-in models for functions where Coverity's
  * assumptions about the modelled function are incorrect for iPXE.
@@ -18,4 +21,9 @@ void copy_from_user ( void *dest, userptr_t src, off_t src_off, size_t len ) {
 time_t mktime ( struct tm *tm ) {
 }
 int getchar ( void ) {
+}
+size_t wcrtomb ( char *buf, wchar_t wc, mbstate_t *ps ) {
+}
+void hmac_init ( struct digest_algorithm *digest, void *digest_ctx,
+		 void *key, size_t *key_len ) {
 }
