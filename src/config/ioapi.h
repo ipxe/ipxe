9fd212d63384e009688c3b38e83621d558583228
Michael Brown <mcb30@ipxe.org>
2021-02-13 19:41:03 +0000

    [cloud] Use PCIAPI_DIRECT for cloud images
    The version of SeaBIOS found on some AWS EC2 instances (observed with
    t3a.nano in eu-west-1) has no support for the INT 1A PCI BIOS calls.
    
    Bring config/ioapi.h into the named-configuration set of headers, and
    specify the use of PCIAPI_DIRECT for CONFIG=cloud, to work around the
    missing PCI BIOS support.
    
    Switching to a different named configuration will now unfortunately
    cause an almost complete rebuild of iPXE.  As described in commit
    c801cb2 ("[build] Allow for named configurations at build time"), this
    is the reason why config/ioapi.h was not originally in the
    named-configuration set of header files.
    
    This rebuild cost is acceptable given that build times are
    substantially faster now than seven years ago, and that very few
    people are likely to be switching named configurations on a regular
    basis.
    
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/config/ioapi.h
+++ src/config/ioapi.h
@@ -14,6 +14,9 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 //#undef	PCIAPI_PCBIOS		/* Access via PCI BIOS */
 //#define	PCIAPI_DIRECT		/* Direct access via Type 1 accesses */
 
+#include <config/named.h>
+#include NAMED_CONFIG(ioapi.h)
 #include <config/local/ioapi.h>
+#include LOCAL_NAMED_CONFIG(ioapi.h)
 
 #endif /* CONFIG_IOAPI_H */
