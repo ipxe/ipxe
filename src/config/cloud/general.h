37739936ea5bbf23c34c578bbc0054ee57dca0cc
Michael Brown <mcb30@ipxe.org>
2021-02-16 10:58:42 +0000

    [cloud] Enable IPv6 and HTTPS in cloud boot images
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/config/cloud/general.h
+++ src/config/cloud/general.h
@@ -1,3 +1,7 @@
+/* Enable IPv6 and HTTPS */
+#define NET_PROTO_IPV6
+#define DOWNLOAD_PROTO_HTTPS
+
 /* Allow retrieval of metadata (such as an iPXE boot script) from
  * Google Compute Engine metadata server.
  */
