7cc305f7b4fb3508e64afa8c4c54bee1fff1f405
Michael Brown <mcb30@ipxe.org>
2023-02-05 18:53:03 +0000

    [efi] Enable NET_PROTO_LLDP by default
    Requested-by: Christian I. Nilsson <nikize@gmail.com>
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/config/general.h
+++ src/config/general.h
@@ -40,7 +40,7 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #define	NET_PROTO_STP		/* Spanning Tree protocol */
 #define	NET_PROTO_LACP		/* Link Aggregation control protocol */
 #define	NET_PROTO_EAPOL		/* EAP over LAN protocol */
-#undef	NET_PROTO_LLDP		/* Link Layer Discovery protocol */
+//#define NET_PROTO_LLDP	/* Link Layer Discovery protocol */
 
 /*
  * PXE support
