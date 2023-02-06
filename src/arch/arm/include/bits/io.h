ef0a6f47920a4fb245f35f1b2e4bcaa7305819cd
Michael Brown <mcb30@ipxe.org>
2023-02-06 12:32:50 +0000

    [ioapi] Move PAGE_SHIFT to bits/io.h
    The PAGE_SHIFT definition is an architectural property, rather than an
    aspect of a particular I/O API implementation (of which, in theory,
    there may be more than one per architecture).
    
    Reflect this by moving the definition to the top-level bits/io.h for
    each architecture.
    
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/arch/arm/include/bits/io.h
+++ src/arch/arm/include/bits/io.h
@@ -9,6 +9,9 @@
 
 FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 
+/** Page shift */
+#define PAGE_SHIFT 12
+
 #include <ipxe/arm_io.h>
 
 #endif /* _BITS_IO_H */
--- src/arch/x86/include/ipxe/x86_io.h
+++ src/arch/x86/include/ipxe/x86_io.h
@@ -28,9 +28,6 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  *
  */
 
-/** Page shift */
-#define PAGE_SHIFT 12
-
 /*
  * Physical<->Bus address mappings
  *
