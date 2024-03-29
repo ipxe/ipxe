165995b7e917a94533799fe43d14f5e3b1fef285
Michael Brown <mcb30@ipxe.org>
2024-03-29 13:03:38 +0000

    [efi] Restructure handling of autoexec.ipxe script
    We currently attempt to obtain the autoexec.ipxe script via early use
    of the EFI_SIMPLE_FILE_SYSTEM_PROTOCOL or EFI_PXE_BASE_CODE_PROTOCOL
    interfaces to obtain an opaque block of memory, which is then
    registered as an image at an appropriate point during our startup
    sequence.  The early use of these existent interfaces allows us to
    obtain the script even if our subsequent actions (e.g. disconnecting
    drivers in order to connect up our own) may cause the script to become
    inaccessible.
    
    This mirrors the approach used under BIOS, where the autoexec.ipxe
    script is provided by the prefix (e.g. as an initrd image when using
    the .lkrn build of iPXE) and so must be copied into a normally
    allocated image from wherever it happens to previously exist in
    memory.
    
    We do not currently have support for downloading an autoexec.ipxe
    script if we were ourselves downloaded via UEFI HTTP boot.
    
    There is an EFI_HTTP_PROTOCOL defined within the UEFI specification,
    but it is so poorly designed as to be unusable for the simple purpose
    of downloading an additional file from the same directory.  It
    provides almost nothing more than a very slim wrapper around
    EFI_TCP4_PROTOCOL (or EFI_TCP6_PROTOCOL).  It will not handle
    redirection, content encoding, retries, or even fundamentals such as
    the Content-Length header, leaving all of this up to the caller.
    
    The UEFI HTTP Boot driver will install an EFI_LOAD_FILE_PROTOCOL
    instance on the loaded image's device handle.  This looks promising at
    first since it provides the LoadFile() API call which is specified to
    accept an arbitrary filename parameter.  However, experimentation (and
    inspection of the code in EDK2) reveals a multitude of problems that
    prevent this from being usable.  Calling LoadFile() will idiotically
    restart the entire DHCP process (and potentially pop up a UI requiring
    input from the user for e.g. a wireless network password).  The
    filename provided to LoadFile() will be ignored.  Any downloaded file
    will be rejected unless it happens to match one of the limited set of
    types expected by the UEFI HTTP Boot driver.  The list of design
    failures and conceptual mismatches is fairly impressive.
    
    Choose to bypass every possible aspect of UEFI HTTP support, and
    instead use our own HTTP client and network stack to download the
    autoexec.ipxe script over a temporary MNP network device.  Since this
    approach works for TFTP as well as HTTP, drop the direct use of
    EFI_PXE_BASE_CODE_PROTOCOL.  For consistency and simplicity, also drop
    the direct use of EFI_SIMPLE_FILE_SYSTEM_PROTOCOL and rely upon our
    existing support to access local files via "file:" URIs.
    
    This approach results in console output during the "iPXE initialising
    devices...ok" message that appears while startup is in progress.
    Remove the trailing "ok" so that this intermediate output appears at a
    sensible location on the screen.  The welcome banner that will be
    printed immediately afterwards provides an indication that startup has
    completed successfully even absent the explicit "ok".
    
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/interface/efi/efiprefix.c
+++ src/interface/efi/efiprefix.c
@@ -81,25 +81,19 @@ EFI_STATUS EFIAPI _efi_start ( EFI_HANDLE image_handle,
 static void efi_init_application ( void ) {
 	EFI_HANDLE device = efi_loaded_image->DeviceHandle;
 	EFI_DEVICE_PATH_PROTOCOL *devpath = efi_loaded_image_path;
-	EFI_DEVICE_PATH_PROTOCOL *filepath = efi_loaded_image->FilePath;
 	struct uri *uri;
 
 	/* Set current working URI from device path, if present */
 	uri = efi_path_uri ( devpath );
 	if ( uri )
 		churi ( uri );
+	uri_put ( uri );
 
 	/* Identify autoboot device, if any */
 	efi_set_autoboot_ll_addr ( device, devpath );
 
 	/* Store cached DHCP packet, if any */
 	efi_cachedhcp_record ( device, devpath );
-
-	/* Load autoexec script, if any */
-	efi_autoexec_load ( device, filepath );
-
-	/* Drop temporary reference to URI */
-	uri_put ( uri );
 }
 
 /** EFI application initialisation function */
@@ -113,6 +107,9 @@ struct init_fn efi_init_application_fn __init_fn ( INIT_NORMAL ) = {
  * @v rootdev		EFI root device
  */
 static int efi_probe ( struct root_device *rootdev __unused ) {
+
+	/* Try loading autoexec script */
+	efi_autoexec_load();
 
 	/* Remove any vetoed drivers */
 	efi_veto();
