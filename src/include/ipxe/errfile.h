c7b76e3adc3b4365aa3b490f24ae22375901c559
Michael Brown <mcb30@ipxe.org>
2024-07-24 14:30:58 +0100

    [gve] Add driver for Google Virtual Ethernet NIC
    The Google Virtual Ethernet NIC (GVE or gVNIC) is found only in Google
    Cloud instances.  There is essentially zero documentation available
    beyond the mostly uncommented source code in the Linux kernel.
    
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/include/ipxe/errfile.h
+++ src/include/ipxe/errfile.h
@@ -224,6 +224,7 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #define ERRFILE_ecam		     ( ERRFILE_DRIVER | 0x00d30000 )
 #define ERRFILE_pcibridge	     ( ERRFILE_DRIVER | 0x00d40000 )
 #define ERRFILE_mnpnet		     ( ERRFILE_DRIVER | 0x00d50000 )
+#define ERRFILE_gve		     ( ERRFILE_DRIVER | 0x00d60000 )
 
 #define ERRFILE_aoe			( ERRFILE_NET | 0x00000000 )
 #define ERRFILE_arp			( ERRFILE_NET | 0x00010000 )
