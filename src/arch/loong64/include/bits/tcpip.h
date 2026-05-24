4906a255e3dde2b96a83e457db323383b0d67a91
Michael Brown <mcb30@ipxe.org>
2026-05-24 23:00:08 +0100

    [loong64] Port the RISC-V optimised TCP/IP checksum implementation
    As with most other assembly code in iPXE, LoongArch64 is sufficiently
    close to RISC-V that a straightforward transcription of the assembly
    language code generally works.
    
    Copy the RISC-V implementation for TCP/IP checksumming, retain the
    register names and ABI, and just adjust the syntax to match
    LoongArch64 requirements.
    
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

---
+++ src/arch/loong64/include/bits/tcpip.h
@@ -0,0 +1,16 @@
+#ifndef _BITS_TCPIP_H
+#define _BITS_TCPIP_H
+
+/** @file
+ *
+ * Transport-network layer interface
+ *
+ */
+
+FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
+FILE_SECBOOT ( PERMITTED );
+
+extern uint16_t tcpip_continue_chksum ( uint16_t partial, const void *data,
+					size_t len );
+
+#endif /* _BITS_TCPIP_H */
