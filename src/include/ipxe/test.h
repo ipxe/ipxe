8362661f9abe777947873b8d557574c847ce7460
Michael Brown <mcb30@ipxe.org>
2026-07-27 14:58:37 +0100

    [test] Simplify the use of inline text for test images
    When specifying the content of a test image (e.g. a MIME archive file)
    using C string literals, there is no easy way to indicate that the
    terminating NUL should be excluded from the byte array.
    
    Provide BINFILE(), TEXTFILE(), and FILE_ARRAY() helper macros that can
    be used to simplify the initialisation of a byte array passed as
    either a raw byte value list or a string literal.  For example:
    
      #define TEST_CASE( name, file ) do {                        \
          static uint8_t name ## _bytes FILE_ARRAY ( file );      \
          ...                                                     \
          } while ( 0 )
    
      TEST_CASE ( test1, BINFILE ( 0x68, 0x65, 0x6c, 0x6c, 0x6f ) );
    
      TEST_CASE ( test2, TEXTFILE ( "hello" ) );
    
    Both of the above TEST_CASE() lines will end up producing a five-byte
    array:
    
      static uint8_t test1_bytes[] = { 0x68, 0x65, 0x6c, 0x6c, 0x6f };
    
      static uint8_t test2_bytes[5] = "hello";
    
    This allows us to remove the stray NUL that otherwise appears at the
    end of any test images that are specified using string literals.
    
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/include/ipxe/test.h
+++ src/include/ipxe/test.h
@@ -46,4 +46,27 @@ extern void test_ok ( int success, const char *file, unsigned int line,
 #define ok( success ) \
 	okx ( success, __FILE__, __LINE__ )
 
+/** Define inline binary file content */
+#define BINFILE( ... ) ( [], ( { __VA_ARGS__ } ) )
+
+/** Define inline text file content */
+#define TEXTFILE( text ) ( [ sizeof (text) - 1 /* strip NUL */ ], (text) )
+
+/** Get file content array declaration */
+#define __file_decl( file ) __file_extract_decl file
+#define __file_extract_decl( decl, init ) decl
+
+/** Get file content array initialiser */
+#define __file_init( file ) __file_extract_init file
+#define __file_extract_init( decl, init ) __file_extract_init_args init
+#define __file_extract_init_args( ... ) __VA_ARGS__
+
+/**
+ * Define a file content array
+ *
+ * @v name		Array name
+ * @v file		File content
+ */
+#define FILE_ARRAY( file ) __file_decl ( file ) = __file_init ( file )
+
 #endif /* _IPXE_TEST_H */
--- src/tests/archive_test.h
+++ src/tests/archive_test.h
@@ -26,22 +26,11 @@ struct archive_test {
 	size_t expected_len;
 };
 
-/** Define inline archive image content */
-#define ARCHIVE(...) { __VA_ARGS__ }
-
-/** Define expected extracted image content
- *
- * For the sake of legibility, we use text strings rather than
- * arbitrary binary data.
- */
-#define EXPECTED( text ) text
-
 /** Define an archive extraction test */
 #define ARCHIVE_TEST( name, TYPE, ARCHIVE_NAME, EXTRACT_NAME,		\
 		      EXPECTED_NAME, ARCHIVE, EXPECTED )		\
-	static const uint8_t name ## _archive[] = ARCHIVE;		\
-	static const uint8_t name ## _expected				\
-		[ sizeof ( EXPECTED ) - 1 ] = EXPECTED;			\
+	static const uint8_t name ## _archive FILE_ARRAY ( ARCHIVE );	\
+	static const uint8_t name ## _expected FILE_ARRAY ( EXPECTED );	\
 	static struct archive_test name = {				\
 		.type = TYPE,						\
 		.archive_name = ARCHIVE_NAME,				\
