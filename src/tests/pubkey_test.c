a2f979a300479a4ab68554d4b35664ed476cd7f0
Michael Brown <mcb30@ipxe.org>
2026-09-02 13:50:25 +0100

    [test] Allow for public-key algorithm tests with incomplete key pairs
    Some public RSA and ECDSA test vector sets provide only the private or
    public half of the key pair, and reuse the same key for multiple tests
    within the set.
    
    Allow public-key tests to omit either half of the key pair, and to
    therefore perform separate tests for encryption, decryption, signature
    generation, and signature verification.
    
    The existing RSA and ECDSA tests (which all include a full key pair)
    are unchanged by this reorganisation.
    
    Signed-off-by: Michael Brown <mcb30@ipxe.org>

--- src/tests/pubkey_test.c
+++ src/tests/pubkey_test.c
@@ -69,42 +69,129 @@ int pubkey_test_get_random ( void *data, size_t len ) {
 }
 
 /**
- * Report public key encryption and decryption test result
+ * Report a public key encryption test result
  *
- * @v test		Public key encryption and decryption test
+ * @v test		Public key encryption/decryption test
  * @v file		Test code file
  * @v line		Test code line
  */
-void pubkey_okx ( struct pubkey_test *test, const char *file,
-		  unsigned int line ) {
-	struct pubkey_algorithm *pubkey = test->pubkey;
-	struct asn1_builder plaintext;
+void pubkey_encrypt_okx ( struct pubkey_encryption_test *test,
+			  const char *file, unsigned int line ) {
+	struct pubkey_algorithm *pubkey = test->key->pubkey;
 	struct asn1_builder ciphertext;
 
-	/* Test key matching */
-	okx ( pubkey_match ( pubkey, &test->private, &test->public ) == 0,
-	      file, line );
+	/* Sanity checks */
+	okx ( test->key->public.len != 0, file, line );
+	okx ( test->ciphertext.len != 0, file, line );
 
 	/* Test encrypting with public key to obtain known ciphertext */
 	ciphertext.data = NULL;
 	ciphertext.len = 0;
 	pubkey_random = test->random;
 	pubkey_random_len = test->random_len;
-	okx ( pubkey_encrypt ( pubkey, &test->public, &test->plaintext,
+	okx ( pubkey_encrypt ( pubkey, &test->key->public, &test->plaintext,
 			       &ciphertext ) == 0, file, line );
 	okx ( pubkey_random_len == 0, file, line );
 	okx ( asn1_compare ( asn1_built ( &ciphertext ),
 			     &test->ciphertext ) == 0, file, line );
 	free ( ciphertext.data );
+}
+
+/**
+ * Report a public key decryption test result
+ *
+ * @v test		Public key encryption/decryption test
+ * @v file		Test code file
+ * @v line		Test code line
+ */
+void pubkey_decrypt_okx ( struct pubkey_encryption_test *test,
+			  const char *file, unsigned int line ) {
+	struct pubkey_algorithm *pubkey = test->key->pubkey;
+	struct asn1_builder plaintext;
+
+	/* Sanity checks */
+	okx ( test->key->private.len != 0, file, line );
+	okx ( test->ciphertext.len != 0, file, line );
 
 	/* Test decrypting with private key to obtain known plaintext */
 	plaintext.data = NULL;
 	plaintext.len = 0;
-	okx ( pubkey_decrypt ( pubkey, &test->private, &test->ciphertext,
+	okx ( pubkey_decrypt ( pubkey, &test->key->private, &test->ciphertext,
 			       &plaintext ) == 0, file, line );
 	okx ( asn1_compare ( asn1_built ( &plaintext ),
 			     &test->plaintext ) == 0, file, line );
 	free ( plaintext.data );
+}
+
+/**
+ * Report a public key encryption failure test result
+ *
+ * @v test		Public key encryption/decryption test
+ * @v file		Test code file
+ * @v line		Test code line
+ */
+void pubkey_encrypt_fail_okx ( struct pubkey_encryption_test *test,
+			       const char *file, unsigned int line ) {
+	struct pubkey_algorithm *pubkey = test->key->pubkey;
+	struct asn1_builder ciphertext;
+
+	/* Test encrypting with public key */
+	ciphertext.data = NULL;
+	ciphertext.len = 0;
+	pubkey_random = test->random;
+	pubkey_random_len = test->random_len;
+	okx ( pubkey_encrypt ( pubkey, &test->key->public, &test->plaintext,
+			       &ciphertext ) != 0, file, line );
+	free ( ciphertext.data );
+}
+
+/**
+ * Report a public key decryption failure test result
+ *
+ * @v test		Public key encryption/decryption test
+ * @v file		Test code file
+ * @v line		Test code line
+ */
+void pubkey_decrypt_fail_okx ( struct pubkey_encryption_test *test,
+			       const char *file, unsigned int line ) {
+	struct pubkey_algorithm *pubkey = test->key->pubkey;
+	struct asn1_builder plaintext;
+
+	/* Test decrypting with private key to obtain known plaintext */
+	plaintext.data = NULL;
+	plaintext.len = 0;
+	okx ( pubkey_decrypt ( pubkey, &test->key->private, &test->ciphertext,
+			       &plaintext ) != 0, file, line );
+	free ( plaintext.data );
+}
+
+/**
+ * Report a public key encryption and decryption test result
+ *
+ * @v test		Public key encryption/decryption test
+ * @v file		Test code file
+ * @v line		Test code line
+ */
+void pubkey_encrypt_decrypt_okx ( struct pubkey_encryption_test *test,
+				  const char *file, unsigned int line ) {
+	struct pubkey_algorithm *pubkey = test->key->pubkey;
+	struct asn1_builder plaintext;
+	struct asn1_builder ciphertext;
+
+	/* Sanity checks */
+	okx ( test->key->private.len != 0, file, line );
+	okx ( test->key->public.len != 0, file, line );
+	okx ( test->ciphertext.len != 0, file, line );
+
+	/* Test key matching */
+	okx ( pubkey_match ( pubkey, &test->key->private,
+			     &test->key->public ) == 0, file, line );
+
+	/* Test encrypting with public key to obtain known ciphertext */
+	pubkey_encrypt_okx ( test, file, line );
+
+	/* Test decrypting with private key to obtain known plaintext */
+	pubkey_decrypt_okx ( test, file, line );
 
 	/* Test encrypting with private key and decrypting with public key */
 	ciphertext.data = NULL;
@@ -113,10 +200,10 @@ void pubkey_okx ( struct pubkey_test *test, const char *file,
 	plaintext.len = 0;
 	pubkey_random = test->random;
 	pubkey_random_len = test->random_len;
-	okx ( pubkey_encrypt ( pubkey, &test->private, &test->plaintext,
+	okx ( pubkey_encrypt ( pubkey, &test->key->private, &test->plaintext,
 			       &ciphertext ) == 0, file, line );
 	okx ( pubkey_random_len == 0, file, line );
-	okx ( pubkey_decrypt ( pubkey, &test->public,
+	okx ( pubkey_decrypt ( pubkey, &test->key->public,
 			       asn1_built ( &ciphertext ),
 			       &plaintext ) == 0, file, line );
 	okx ( asn1_compare ( asn1_built ( &plaintext ),
@@ -131,10 +218,10 @@ void pubkey_okx ( struct pubkey_test *test, const char *file,
 	plaintext.len = 0;
 	pubkey_random = test->random;
 	pubkey_random_len = test->random_len;
-	okx ( pubkey_encrypt ( pubkey, &test->public, &test->plaintext,
+	okx ( pubkey_encrypt ( pubkey, &test->key->public, &test->plaintext,
 			       &ciphertext ) == 0, file, line );
 	okx ( pubkey_random_len == 0, file, line );
-	okx ( pubkey_decrypt ( pubkey, &test->private,
+	okx ( pubkey_decrypt ( pubkey, &test->key->private,
 			       asn1_built ( &ciphertext ),
 			       &plaintext ) == 0, file, line );
 	okx ( asn1_compare ( asn1_built ( &plaintext ),
@@ -144,26 +231,73 @@ void pubkey_okx ( struct pubkey_test *test, const char *file,
 }
 
 /**
- * Report public key signature test result
+ * Report a public key signature test result
  *
- * @v test		Public key signature test
+ * @v test		Public key signature/verification test
  * @v file		Test code file
  * @v line		Test code line
  */
-void pubkey_sign_okx ( struct pubkey_sign_test *test, const char *file,
-		       unsigned int line ) {
-	struct pubkey_algorithm *pubkey = test->pubkey;
+void pubkey_sign_okx ( struct pubkey_signature_test *test,
+		       const char *file, unsigned int line ) {
+	struct pubkey_algorithm *pubkey = test->key->pubkey;
+	struct digest_algorithm *digest = test->digest;
+	uint8_t digestctx[digest->ctxsize];
+	uint8_t digestout[digest->digestsize];
+	struct asn1_builder builder = { NULL, 0 };
+
+	/* Sanity checks */
+	okx ( test->key->private.len != 0, file, line );
+	okx ( test->key->public.len != 0, file, line );
+	okx ( test->signature.len != 0, file, line );
+
+	/* Test key matching */
+	okx ( pubkey_match ( pubkey, &test->key->private,
+			     &test->key->public ) == 0, file, line );
+
+	/* Construct digest over plaintext */
+	digest_init ( digest, digestctx );
+	digest_update ( digest, digestctx, test->plaintext,
+			test->plaintext_len );
+	digest_final ( digest, digestctx, digestout );
+
+	/* Test signing using private key */
+	pubkey_random = test->random;
+	pubkey_random_len = test->random_len;
+	okx ( pubkey_sign ( pubkey, &test->key->private, digest, digestout,
+			    &builder ) == 0, file, line );
+	okx ( pubkey_random_len == 0, file, line );
+	okx ( builder.len != 0, file, line );
+	okx ( asn1_compare ( asn1_built ( &builder ), &test->signature ) == 0,
+	      file, line );
+
+	/* Test verification of constructed signature */
+	okx ( pubkey_verify ( pubkey, &test->key->public, digest, digestout,
+			      asn1_built ( &builder ) ) == 0, file, line );
+
+	/* Free signature */
+	free ( builder.data );
+}
+
+/**
+ * Report a public key verification test result
+ *
+ * @v test		Public key signature/verification test
+ * @v file		Test code file
+ * @v line		Test code line
+ */
+void pubkey_verify_okx ( struct pubkey_signature_test *test,
+			 const char *file, unsigned int line ) {
+	struct pubkey_algorithm *pubkey = test->key->pubkey;
 	struct digest_algorithm *digest = test->digest;
 	uint8_t digestctx[digest->ctxsize];
 	uint8_t digestout[digest->digestsize];
 	uint8_t signature[test->signature.len];
 	struct asn1_cursor cursor = { signature, sizeof ( signature ) };
-	struct asn1_builder builder = { NULL, 0 };
 	uint8_t *bad;
 
-	/* Test key matching */
-	okx ( pubkey_match ( pubkey, &test->private, &test->public ) == 0,
-	      file, line );
+	/* Sanity checks */
+	okx ( test->key->public.len != 0, file, line );
+	okx ( test->signature.len != 0, file, line );
 
 	/* Construct digest over plaintext */
 	digest_init ( digest, digestctx );
@@ -172,33 +306,68 @@ void pubkey_sign_okx ( struct pubkey_sign_test *test, const char *file,
 	digest_final ( digest, digestctx, digestout );
 
 	/* Test verification using public key */
-	okx ( pubkey_verify ( pubkey, &test->public, digest, digestout,
+	okx ( pubkey_verify ( pubkey, &test->key->public, digest, digestout,
 			      &test->signature ) == 0, file, line );
 
 	/* Test verification failure of modified signature */
 	memcpy ( signature, test->signature.data, sizeof ( signature ) );
 	bad = ( signature + ( sizeof ( signature ) / 2 ) );
 	*bad ^= 0x40;
-	okx ( pubkey_verify ( pubkey, &test->public, digest, digestout,
+	okx ( pubkey_verify ( pubkey, &test->key->public, digest, digestout,
 			      &cursor ) != 0, file, line );
 	*bad ^= 0x40;
-	okx ( pubkey_verify ( pubkey, &test->public, digest, digestout,
+	okx ( pubkey_verify ( pubkey, &test->key->public, digest, digestout,
 			      &cursor ) == 0, file, line );
+}
 
-	/* Test signing using private key */
-	pubkey_random = test->random;
-	pubkey_random_len = test->random_len;
-	okx ( pubkey_sign ( pubkey, &test->private, digest, digestout,
-			    &builder ) == 0, file, line );
-	okx ( pubkey_random_len == 0, file, line );
-	okx ( builder.len != 0, file, line );
-	okx ( asn1_compare ( asn1_built ( &builder ), &test->signature ) == 0,
-	      file, line );
+/**
+ * Report a public key verification failure test result
+ *
+ * @v test		Public key signature/verification test
+ * @v file		Test code file
+ * @v line		Test code line
+ */
+void pubkey_verify_fail_okx ( struct pubkey_signature_test *test,
+			      const char *file, unsigned int line ) {
+	struct pubkey_algorithm *pubkey = test->key->pubkey;
+	struct digest_algorithm *digest = test->digest;
+	uint8_t digestctx[digest->ctxsize];
+	uint8_t digestout[digest->digestsize];
 
-	/* Test verification of constructed signature */
-	okx ( pubkey_verify ( pubkey, &test->public, digest, digestout,
-			      asn1_built ( &builder ) ) == 0, file, line );
+	/* Construct digest over plaintext */
+	digest_init ( digest, digestctx );
+	digest_update ( digest, digestctx, test->plaintext,
+			test->plaintext_len );
+	digest_final ( digest, digestctx, digestout );
 
-	/* Free signature */
-	free ( builder.data );
+	/* Test verification using public key */
+	okx ( pubkey_verify ( pubkey, &test->key->public, digest, digestout,
+			      &test->signature ) != 0, file, line );
+}
+
+/**
+ * Report a public key signature and verification test result
+ *
+ * @v test		Public key signature/verification test
+ * @v file		Test code file
+ * @v line		Test code line
+ */
+void pubkey_sign_verify_okx ( struct pubkey_signature_test *test,
+			      const char *file, unsigned int line ) {
+	struct pubkey_algorithm *pubkey = test->key->pubkey;
+
+	/* Sanity checks */
+	okx ( test->key->private.len != 0, file, line );
+	okx ( test->key->public.len != 0, file, line );
+	okx ( test->signature.len != 0, file, line );
+
+	/* Test key matching */
+	okx ( pubkey_match ( pubkey, &test->key->private,
+			     &test->key->public ) == 0, file, line );
+
+	/* Test signature */
+	pubkey_sign_okx ( test, file, line );
+
+	/* Test verification */
+	pubkey_verify_okx ( test, file, line );
 }
--- src/tests/pubkey_test.h
+++ src/tests/pubkey_test.h
@@ -7,7 +7,7 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #include <ipxe/crypto.h>
 #include <ipxe/test.h>
 
-/** A public-key encryption and decryption test */
+/** A public-key test */
 struct pubkey_test {
 	/** Public-key algorithm */
 	struct pubkey_algorithm *pubkey;
@@ -15,6 +15,12 @@ struct pubkey_test {
 	const struct asn1_cursor private;
 	/** Public key */
 	const struct asn1_cursor public;
+};
+
+/** A public-key encryption/decryption test */
+struct pubkey_encryption_test {
+	/** Test key */
+	const struct pubkey_test *key;
 	/** Random data input */
 	const void *random;
 	/** Random data input length */
@@ -25,14 +31,10 @@ struct pubkey_test {
 	const struct asn1_cursor ciphertext;
 };
 
-/** A public-key signature test */
-struct pubkey_sign_test {
-	/** Public-key algorithm */
-	struct pubkey_algorithm *pubkey;
-	/** Private key */
-	const struct asn1_cursor private;
-	/** Public key */
-	const struct asn1_cursor public;
+/** A public-key signature/verification test */
+struct pubkey_signature_test {
+	/** Test key */
+	const struct pubkey_test *key;
 	/** Random data input */
 	const void *random;
 	/** Random data input length */
@@ -66,7 +68,7 @@ struct pubkey_sign_test {
 #define SIGNATURE(...) { __VA_ARGS__ }
 
 /**
- * Define a public-key encryption and decryption test
+ * Define a public-key test
  *
  * @v name		Test name
  * @v PUBKEY		Public-key algorithm
@@ -77,13 +79,9 @@ struct pubkey_sign_test {
  * @v CIPHERTEXT	Ciphertext
  * @ret test		Encryption and decryption test
  */
-#define PUBKEY_TEST( name, PUBKEY, PRIVATE, PUBLIC, RANDOM, PLAINTEXT,	\
-		     CIPHERTEXT )					\
+#define PUBKEY_TEST( name, PUBKEY, PRIVATE, PUBLIC )			\
 	static const uint8_t name ## _private[] = PRIVATE;		\
 	static const uint8_t name ## _public[] = PUBLIC;		\
-	static const uint8_t name ## _random[] = RANDOM;		\
-	static const uint8_t name ## _plaintext[] = PLAINTEXT;		\
-	static const uint8_t name ## _ciphertext[] = CIPHERTEXT;	\
 	static struct pubkey_test name = {				\
 		.pubkey = PUBKEY,					\
 		.private = {						\
@@ -94,6 +92,25 @@ struct pubkey_sign_test {
 			.data = name ## _public,			\
 			.len = sizeof ( name ## _public ),		\
 		},							\
+	}
+
+/**
+ * Define a public-key encryption/decryption test
+ *
+ * @v name		Test name
+ * @v KEY		Test key
+ * @v RANDOM		Random data
+ * @v PLAINTEXT		Plaintext
+ * @v CIPHERTEXT	Ciphertext
+ * @ret test		Encryption and decryption test
+ */
+#define PUBKEY_ENCRYPTION_TEST( name, KEY, RANDOM, PLAINTEXT,		\
+				CIPHERTEXT )				\
+	static const uint8_t name ## _random[] = RANDOM;		\
+	static const uint8_t name ## _plaintext[] = PLAINTEXT;		\
+	static const uint8_t name ## _ciphertext[] = CIPHERTEXT;	\
+	static struct pubkey_encryption_test name = {			\
+		.key = KEY,						\
 		.random = name ## _random,				\
 		.random_len = sizeof ( name ## _random ),		\
 		.plaintext = {						\
@@ -107,35 +124,23 @@ struct pubkey_sign_test {
 	}
 
 /**
- * Define a public-key signature test
+ * Define a public-key signature/verification test
  *
  * @v name		Test name
- * @v PUBKEY		Public-key algorithm
- * @v PRIVATE		Private key
- * @v PUBLIC		Public key
+ * @v KEY		Test key
  * @v RANDOM		Random data
  * @v PLAINTEXT		Plaintext
  * @v DIGEST		Digest algorithm
  * @v SIGNATURE		Signature
  * @ret test		Signature test
  */
-#define PUBKEY_SIGN_TEST( name, PUBKEY, PRIVATE, PUBLIC, RANDOM,	\
-			  PLAINTEXT, DIGEST, SIGNATURE )		\
-	static const uint8_t name ## _private[] = PRIVATE;		\
-	static const uint8_t name ## _public[] = PUBLIC;		\
+#define PUBKEY_SIGNATURE_TEST( name, KEY, RANDOM, PLAINTEXT, DIGEST,	\
+			       SIGNATURE )				\
 	static const uint8_t name ## _random[] = RANDOM;		\
 	static const uint8_t name ## _plaintext[] = PLAINTEXT;		\
 	static const uint8_t name ## _signature[] = SIGNATURE;		\
-	static struct pubkey_sign_test name = {				\
-		.pubkey = PUBKEY,					\
-		.private = {						\
-			.data = name ## _private,			\
-			.len = sizeof ( name ## _private ),		\
-		},							\
-		.public = {						\
-			.data = name ## _public,			\
-			.len = sizeof ( name ## _public ),		\
-		},							\
+	static struct pubkey_signature_test name = {			\
+		.key = KEY,						\
 		.random = name ## _random,				\
 		.random_len = sizeof ( name ## _random ),		\
 		.plaintext = name ## _plaintext,			\
@@ -148,25 +153,96 @@ struct pubkey_sign_test {
 	}
 
 extern int pubkey_test_get_random ( void *data, size_t len );
-extern void pubkey_okx ( struct pubkey_test *test,
-			 const char *file, unsigned int line );
-extern void pubkey_sign_okx ( struct pubkey_sign_test *test,
+extern void pubkey_encrypt_okx ( struct pubkey_encryption_test *test,
+				 const char *file, unsigned int line );
+extern void pubkey_decrypt_okx ( struct pubkey_encryption_test *test,
+				 const char *file, unsigned int line );
+extern void pubkey_encrypt_fail_okx ( struct pubkey_encryption_test *test,
+				      const char *file, unsigned int line );
+extern void pubkey_decrypt_fail_okx ( struct pubkey_encryption_test *test,
+				      const char *file, unsigned int line );
+extern void pubkey_encrypt_decrypt_okx ( struct pubkey_encryption_test *test,
+					 const char *file,
+					 unsigned int line );
+extern void pubkey_sign_okx ( struct pubkey_signature_test *test,
 			      const char *file, unsigned int line );
+extern void pubkey_verify_okx ( struct pubkey_signature_test *test,
+				const char *file, unsigned int line );
+extern void pubkey_verify_fail_okx ( struct pubkey_signature_test *test,
+				     const char *file, unsigned int line );
+extern void pubkey_sign_verify_okx ( struct pubkey_signature_test *test,
+				     const char *file, unsigned int line );
+
+/**
+ * Report a public key encryption test result
+ *
+ * @v test		Public key encryption/decryption test
+ */
+#define pubkey_encrypt_ok( test ) \
+	pubkey_encrypt_okx ( test, __FILE__, __LINE__ )
+
+/**
+ * Report a public key decryption test result
+ *
+ * @v test		Public key encryption/decryption test
+ */
+#define pubkey_decrypt_ok( test ) \
+	pubkey_decrypt_okx ( test, __FILE__, __LINE__ )
+
+/**
+ * Report a public key encryption failure test result
+ *
+ * @v test		Public key encryption/decryption test
+ */
+#define pubkey_encrypt_fail_ok( test ) \
+	pubkey_encrypt_fail_okx ( test, __FILE__, __LINE__ )
+
+/**
+ * Report a public key decryption failure test result
+ *
+ * @v test		Public key decryption/decryption test
+ */
+#define pubkey_decrypt_fail_ok( test ) \
+	pubkey_decrypt_fail_okx ( test, __FILE__, __LINE__ )
 
 /**
  * Report a public key encryption and decryption test result
  *
- * @v test		Public key encryption and decryption test
+ * @v test		Public key encryption/decryption test
  */
-#define pubkey_ok( test ) \
-	pubkey_okx ( test, __FILE__, __LINE__ )
+#define pubkey_encrypt_decrypt_ok( test ) \
+	pubkey_encrypt_decrypt_okx ( test, __FILE__, __LINE__ )
 
 /**
  * Report a public key signature test result
  *
- * @v test		Public key signature test
+ * @v test		Public key signature/verification test
  */
 #define pubkey_sign_ok( test ) \
 	pubkey_sign_okx ( test, __FILE__, __LINE__ )
+
+/**
+ * Report a public key verification test result
+ *
+ * @v test		Public key signature/verification test
+ */
+#define pubkey_verify_ok( test ) \
+	pubkey_verify_okx ( test, __FILE__, __LINE__ )
+
+/**
+ * Report a public key verification failure test result
+ *
+ * @v test		Public key signature/verification test
+ */
+#define pubkey_verify_fail_ok( test ) \
+	pubkey_verify_fail_okx ( test, __FILE__, __LINE__ )
+
+/**
+ * Report a public key signature and verification test result
+ *
+ * @v test		Public key signature/verification test
+ */
+#define pubkey_sign_verify_ok( test ) \
+	pubkey_sign_verify_okx ( test, __FILE__, __LINE__ )
 
 #endif /* _PUBKEY_TEST_H */
